3ce4b331c9d4a58c7a5d5258474c1e1db2640cf4
adufilie <andy.dufilie@gmail.com>
2011-10-21 09:22:14 -0400

    Added dynamic query capability (session state only). Bug fix for DataTableManager 'test all queries' button.
    Change-Id: I55f61f48e86930bf1b11a25ab875112b9b3f76b8

--- WeaveServices/src/weave/config/ISQLConfig.java
+++ WeaveServices/src/weave/config/ISQLConfig.java
@@ -274,7 +274,8 @@ public interface ISQLConfig
 		// this information should not be made available to client programs
 		public static final String CONNECTION = "connection";
 		public static final String SQLQUERY = "sqlQuery";
-		public static final String SQLRESULT = "sqlResult"; // sqlResult is only transmitted to the client, never stored in the database
+		public static final String SQLPARAMS = "sqlParams"; // only transmitted from client to server, never stored in the database
+		public static final String SQLRESULT = "sqlResult"; // only transmitted from server to client, never stored in the database
 
 		// Metadata includes everything that end-users are allowed to see.
 		// Metadata should not contain any information related to the SQL
