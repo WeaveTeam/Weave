ca641350473c872a0e52a8082064d06cc8d60ef6
patchcr <patchcr@gmail.com>
2013-06-13 14:37:38 -0400

    Fixed Timing of HTML Imports. Applied Darkness CSS. Edited some CSS for the Panels.

--- AWS/ROOT/js/includes.js
+++ AWS/ROOT/js/includes.js
@@ -14,211 +14,210 @@
 
 require.config({
 	paths : {
-		//'jquery' : 'js/jquery-1.4.2.min',
+		// 'jquery' : 'js/jquery',
 		'swfobject' : 'swfobject'
 	}
 });
 
-require([ "js/jquery-1.4.2.min.js", "js/jquery.cookie.js", "js/jquery.sizes.js",
-		"js/jlayout.border.js", "swfobject", "js/jquery-ui-1.8.custom.min.js",
-		"js/jquery.jlayout.js", "js/ui.panel.js" ], function($) {
-	
-	// JLayout script
-	jQuery(function($) {
-		var container = $('.layout');
+require(
+		[ "js/jquery-1.9.1.js","text!../src/GenericPanel.html!strip", "js/jquery-ui-1.10.3.custom.js",
+				"js/jlayout.border.js", "js/jquery.jlayout.js",
+				"js/jquery.sizes.js", "swfobject" ],
+		function($, html, $, jLayout) {
 
-		function layout() {
-			container.layout({
-				resize : false,
-				type : 'border',
-				vgap : 8,
-				hgap : 8
+			// JLayout script
+			jQuery(function($) {
+				var i = $('#panel1');
+				i.append(html);
+				
+				var container = $('.layout');
+
+				function borderLayout() {
+					container.layout({
+						resize : false,
+						type : 'border',
+						vgap : 8,
+						hgap : 8
+					});
+				}
+
+				function gridLayout() {
+					$('.center').layout({
+						type : 'grid',
+						resize : false,
+						columns : 3,
+						fill : 'horizontal'
+					});
+					var pan = $(".varPanel");
+						pan.find("span.ui-icon-minusthick").remove();
+						pan.addClass(
+									"ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+							.find(".varPanel-header")
+							.addClass("ui-widget-header ui-corner-all")
+							.prepend(
+									"<span class='ui-icon ui-icon-minusthick' style='float:left'></span>")
+							.end().find(".varPanel-content");
+					$(".varPanel-header .ui-icon").click(
+							function() {
+								$(this).toggleClass("ui-icon-minusthick")
+										.toggleClass("ui-icon-plusthick");
+								$(this).parents(".varPanel:first").find(
+										".varPanel-content").toggle();
+							});
+				}
+
+				$('.north').resizable({
+					handles : 's',
+					stop : borderLayout,
+					resize : borderLayout
+				});
+
+				$('.south').resizable({
+					handles : 'n',
+					stop : borderLayout,
+					resize : borderLayout
+				});
+
+				/*
+				 * $('.east').resizable({ handles : 'w', stop : borderLayout,
+				 * resize : borderLayout });
+				 */
+
+				$('.west').resizable({
+					handles : 'e',
+					stop : borderLayout,
+					resize : borderLayout
+				});
+				/*$('.varPanel').draggable({
+					connectToSortable : "#east",
+				});*/
+				gridLayout();
+
+				/*
+				 * $('#panel5').panel({ // 'draggable' : false, 'stackable' :
+				 * false, 'collapseType' : 'slide-right' });
+				 */
+
+				$(window).resize(borderLayout);
+
+				borderLayout();
+				borderLayout();
+				$('.sortable').sortable();
+				/*$("#center>.sortable").sortable({
+					connectWith : '#east>.sortable'
+				});
+				$("#east>.sortable").sortable({
+					connectWith : '#center>.sortable'
+				});*/
+				$(".sortable").disableSelection();
+
+				gridLayout();
 			});
-		}
 
-		$('.north').resizable({
-			handles : 's',
-			stop : layout,
-			resize : layout
+			// Panel Contents
+			jQuery(function($) {
+				$(document).ready(
+						function() {
+							
+							var countryCombobox = $('#countryCombobox');
+							countryCombobox.append($("<option/>").val('').text(
+									"Select one..."));
+							countryCombobox.append($("<option/>").val('US')
+									.text("United States"));
+
+							var stateCombobox = $('#stateCombobox');
+							stateCombobox.append($("<option/>").val('')
+									.text(""));
+							stateCombobox.append($("<option/>").val('MA').text(
+									"Massachussetts"));
+							stateCombobox.append($("<option/>").val('RI').text(
+									"Rhode Island"));
+							stateCombobox.append($("<option/>").val('CO').text(
+									"Connecticut"));
+							stateCombobox.append($("<option/>").val('TX').text(
+									"Texas"));
+							stateCombobox.append($("<option/>").val('WA').text(
+									"Washington"));
+							stateCombobox.append($("<option/>").val('OR').text(
+									"Oregon"));
+							stateCombobox.append($("<option/>").val('IL').text(
+									"Illinois"));
+
+							var countyCombobox = $('#countyCombobox');
+							countyCombobox.append($("<option/>").val('').text(
+									""));
+							countyCombobox.append($("<option/>").val(
+									'Worcester').text("Worcester"));
+							countyCombobox.append($("<option/>").val(
+									'Middlesex').text("Middlesex"));
+							countyCombobox.append($("<option/>").val('Suffolk')
+									.text("Suffolk"));
+							countyCombobox.append($("<option/>")
+									.val('Plymouth').text("Plymouth"));
+
+							var cityCombobox = $('#cityCombobox');
+							cityCombobox
+									.append($("<option/>").val('').text(""));
+							cityCombobox.append($("<option/>").val('Lowell')
+									.text("Lowell"));
+
+							var startYearCombobox = $('#startYearCombobox');
+							startYearCombobox.append($("<option/>").val('2010')
+									.text("2010"));
+
+							var EndYearCombobox = $('#EndYearCombobox');
+							EndYearCombobox.append($("<option/>").val('2012')
+									.text("2012"));
+							EndYearCombobox.append($("<option/>").val('2013')
+									.text("2013"));
+
+							var timeTypeCombobox = $('#timeTypeCombobox');
+							timeTypeCombobox.append($("<option/>").val(
+									'byQuarter').text("By Quarter"));
+							timeTypeCombobox.append($("<option/>")
+									.val('byYear').text("By Year"));
+
+							$('#panel2SubmitButton').button().click(function() {
+								console.log('function 2 called');
+							});
+
+							$('#panel2MapButton').button().click(function() {
+								console.log('function 3 called');
+							});
+
+							$('#panel3SubmitButton').button().click(function() {
+								console.log('function called');
+							});
+
+							$('#panel4SubmitButton').button().click(function() {
+								console.log('function called');
+							});
+
+							$('#panel5SubmitButton').button().click(function() {
+								testServerQuery('getRResult');
+							});
+
+							$('#panel6ImportButton').button().click(function() {
+								console.log('function called');
+							});
+
+							$('#panel6SaveButton').button().click(function() {
+								console.log('function called');
+							});
+
+							$('#panel6EditButton').button().click(function() {
+								console.log('function called');
+							});
+							$(".box").sortable({
+								connectWith : ".box"
+							});
+
+						});// end of panel contents
+			});
 		});
 
-		$('.south').resizable({
-			handles : 'n',
-			stop : layout,
-			resize : layout
-		});
+/*require([ "jquery", "text!../src/GenericPanel.html!strip" ], function($, html) {
+	var i = $('#panel1');
+	i.append(html);
 
-		$('.east').resizable({
-			handles : 'w',
-			stop : layout,
-			resize : layout
-		});
-
-		$('.west').resizable({
-			handles : 'e',
-			stop : layout,
-			resize : layout
-		});
-		$('#panel1').panel({
-			// 'draggable' : true,
-			'collapsible' : true,
-			'collapsed' : true
-		});
-		$('#panel2').panel({
-			// 'draggable' : true,
-			'collapsible' : true,
-			'collapsed' : true
-		});
-		$('#panel3').panel({
-			// 'draggable' : true,
-			'collapsible' : true,
-			'collapsed' : true
-		});
-		$('#panel4').panel({
-			// 'draggable' : true,
-			'collapsible' : true,
-			'collapsed' : true
-		});
-		$('#panel5').panel({
-			// 'draggable' : false,
-			'stackable' : false,
-			'collapseType' : 'slide-right'
-		});
-		$('#panel6').panel({
-			// 'draggable' : true,
-			'collapsible' : true,
-			'collapsed' : true
-		});
-
-		$(window).resize(layout);
-
-		layout();
-		layout();
-		
-		$("#sortable").sortable();
-		$("#sortable").disableSelection();
-	});
-	
-	/*$(function($) {
-		
-	});*/
-
-	// Panel Contents
-	jQuery(function($){
-		$(document)
-		.ready(
-			function() {
-				var combobox1 = $('#combobox1');
-				combobox1.append($("<option/>").val('').text(
-						"Select one..."));
-				combobox1.append($("<option/>").val('CSVDataSource1')
-						.text("CSVDataSource1"));
-				combobox1.append($("<option/>").val('CSVDataSource1')
-						.text("Obesity"));
-				combobox1.append($("<option/>").val('database').text(
-						"(Data from server)"));
-
-				var countryCombobox = $('#countryCombobox');
-				countryCombobox.append($("<option/>").val('').text(
-						"Select one..."));
-				countryCombobox.append($("<option/>").val('US').text(
-						"United States"));
-
-				var stateCombobox = $('#stateCombobox');
-				stateCombobox.append($("<option/>").val('').text(""));
-				stateCombobox.append($("<option/>").val('MA').text(
-						"Massachussetts"));
-				stateCombobox.append($("<option/>").val('RI').text(
-						"Rhode Island"));
-				stateCombobox.append($("<option/>").val('CO').text(
-						"Connecticut"));
-				stateCombobox.append($("<option/>").val('TX').text(
-						"Texas"));
-				stateCombobox.append($("<option/>").val('WA').text(
-						"Washington"));
-				stateCombobox.append($("<option/>").val('OR').text(
-						"Oregon"));
-				stateCombobox.append($("<option/>").val('IL').text(
-						"Illinois"));
-
-				var countyCombobox = $('#countyCombobox');
-				countyCombobox.append($("<option/>").val('').text(""));
-				countyCombobox.append($("<option/>").val('Worcester')
-						.text("Worcester"));
-				countyCombobox.append($("<option/>").val('Middlesex')
-						.text("Middlesex"));
-				countyCombobox.append($("<option/>").val('Suffolk')
-						.text("Suffolk"));
-				countyCombobox.append($("<option/>").val('Plymouth')
-						.text("Plymouth"));
-
-				var cityCombobox = $('#cityCombobox');
-				cityCombobox.append($("<option/>").val('').text(""));
-				cityCombobox.append($("<option/>").val('Lowell').text(
-						"Lowell"));
-
-				var startYearCombobox = $('#startYearCombobox');
-				startYearCombobox.append($("<option/>").val('2010')
-						.text("2010"));
-
-				var EndYearCombobox = $('#EndYearCombobox');
-				EndYearCombobox.append($("<option/>").val('2012').text(
-						"2012"));
-				EndYearCombobox.append($("<option/>").val('2013').text(
-						"2013"));
-
-				var timeTypeCombobox = $('#timeTypeCombobox');
-				timeTypeCombobox.append($("<option/>").val('byQuarter')
-						.text("By Quarter"));
-				timeTypeCombobox.append($("<option/>").val('byYear')
-						.text("By Year"));
-
-				$('#centerPanel').panel({
-					width : '83.10%',
-				});
-
-				$('#panel1SubmitButton').button().click(function() {
-					console.log('function called');
-				});
-
-				$('#panel2SubmitButton').button().click(function() {
-					console.log('function 2 called');
-				});
-
-				$('#panel2MapButton').button().click(function() {
-					console.log('function 3 called');
-				});
-
-				$('#panel3SubmitButton').button().click(function() {
-					console.log('function called');
-				});
-
-				$('#panel4SubmitButton').button().click(function() {
-					console.log('function called');
-				});
-
-				$('#panel5SubmitButton').button().click(function() {
-					testServerQuery('getRResult');
-				});
-
-				$('#panel6ImportButton').button().click(function() {
-					console.log('function called');
-				});
-
-				$('#panel6SaveButton').button().click(function() {
-					console.log('function called');
-				});
-
-				$('#panel6EditButton').button().click(function() {
-					console.log('function called');
-				});
-				$(".box").sortable({
-					connectWith : ".box"
-				});
-				$('#Weave').panel({
-					width : '83.10%',
-					draggable : true,
-				});
-			});//end of panel contents
-	});
-});
+});*/
