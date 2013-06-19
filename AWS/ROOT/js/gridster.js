1a4ca5e2a4ebdaacc6d7a2847418f6c47fb6b581
Franck Kamayou <franckamayou@gmail.com>
2013-06-18 23:17:21 -0400

    roundtrip demo

--- AWS/ROOT/js/gridster.js
+++ AWS/ROOT/js/gridster.js
@@ -2,6 +2,7 @@ require.config({
   paths : {
     'jquery' : 'jquery-1.9.1',
     'jquery-ui' : 'jquery-ui-1.10.3.custom'
+    
   },
   shim : {
     'jquery.gridster' : [ 'jquery' ],
@@ -51,10 +52,12 @@ require(
                 
                 $('#scriptButton').button().click(function() {
                   // enter run script here
+                	testServerQuery('getRResult');
                 });
                 
                 $('#callWeaveButton').button().click(function() {
                   // enter function here for calling weave
+                	scriptOneQuery('getScriptOneResult');
                 });
 
                 $('.portlet')
@@ -97,18 +100,21 @@ require(
                 dataCombobox.append($("<option/>").val('script2')
                           .text("Cars.csv"));
                 
-                $('#weaveDialog').dialog({
-                  autoOpen : false,
-                  height : 500,
-                  width : 750,
-                  modal : true,
-                  buttons : {
-                    Close : function() {
-                      // handleCancelOption();
-                      $(this).dialog("close");
-                    },
-                  }
-                })
+                var dialogOptions = {
+                        "autoOpen": false,
+                        "width": 950,
+                		"height": 700,
+                		"modal" : false,
+                		buttons : {
+                			"Export" : function() {
+                				console.log("Save button clicked");
+                			},
+                		
+                		}
+                };
+                
+                $('#weaveDialog').dialog(dialogOptions);
+             
                 $('#weaveButton').button().click(function() {
                   $('#weaveDialog').dialog("open");
                 });
@@ -125,76 +131,175 @@ require(
                 });
                 
               }));
-      // available methods are listed here:
-      // http://ivpr.github.io/Weave-Binaries/javadoc/weave/servlets/DataService.html
-      function queryDataService(method, params, resultHandler, queryId) {
-        // console.log('queryDataService ',method,' ',params);
-        var url = '/WeaveServices/DataService';
-        var request = {
-          jsonrpc : "2.0",
-          id : queryId || "no_id",
-          method : method,
-          params : params
-        };
-        $.post(url, JSON.stringify(request), handleResponse, "json");
 
-        function handleResponse(response) {
-          if (response.error)
-            console.log(JSON.stringify(response, null, 3));
-          else
-            resultHandler(response.result, queryId);
-        }
-      }
-      function test1() {
-        var tableId = 107945;
-        var name = 'Percent Obese (BMI >= 30)';
+              
+// Disable caching of AJAX responses
+$.ajaxSetup({
+    cache: false
+});
 
-        // get the IDs of all the fields in a table
-        queryDataService(
-            'getEntityChildIds',
-            [ tableId ],
-            function(ids) {
+//calling Rservice on Weave
+function queryRService(method,params,callback,queryID)
+{
+	$('#scriptResults').html("<p>" + JSON.stringify('queryRService',method,params));
+	var url = '/WeaveServices/RService';
+	var request = {
+					jsonrpc:"2.0",
+					id:queryID || "no_id",
+					method : method,
+					params : params
+	};
+	
+	$.post(url,JSON.stringify(request), callback, "json");
+	//resulttextarea.value = 'Awaiting Response for ' + method + ' request....';
+}
 
-              console.log("test1 received child ids: ", ids);
+//---------------------------------called by scriptOneButton BEGIN--------------------------
 
-              // get the metadata for each child
-              queryDataService(
-                  'getEntitiesById',
-                  [ ids ],
-                  function(entities) {
+function scriptOneQuery(secondMethodName)
+{
+	var dataset = "Obesity.csv";
+	var rRoutine = "Normalization.R";
+	
+	queryRService(
+	//method
+	'runScriptOnCSVOnServer',
+	{
+		//params
+		queryObject:[dataset,rRoutine],
+		
+	},
+	//callback
+	handleScriptOneResult
+	);
+	
+	function handleScriptOneResult(response)
+	{
+		if(response.error)
+		{
+			$('#scriptResults').html(JSON.stringify(response,null,3));
+			return;
+		}
+		else
+		{
+			var rResult = response.result;
+			$('#scriptResults').html("calling set CSVSource");
+			setCSV2Source(rResult);
+			$('#scriptResults').html("retrieved " + rResult.length + "results");
+			return;
+		}
+		
+		
+	}
+	
+	
+	function setCSV2Source(rResult)
+	{
+		$('#scriptResults').html(rResult[0].value);
+		var weave = document.getElementById('weave');
+		weave.path('MyNORMDataSource').request('CSVDataSource').vars({data: rResult[0].value}).exec('setCSVDataString(data)');
+		weave.requestObject(['DTT'], 'DataTableTool');
+		
+		
+		
+	}
+}
+//---------------------------------called by scriptOneButton END---------------------------
 
-                    console.log("test1 received ",
-                        entities.length, " entities");
 
-                    // filter by name
-                    entities = entities
-                        .filter(function(e) {
-                          return e.publicMetadata.name == name;
-                        });
-                    console.log("test1 filtered ",
-                        entities.length, " entities");
-                    entities.sort(sortBy('publicMetadata',
-                        'year'));
 
-                    console
-                        .log(
-                            "test1 filtered by name, sorted by year: ",
-                            entities);
-                    console
-                        .log(
-                            "test1 corresponding years: ",
-                            entities
-                                .map(function(e) {
-                                  return e.publicMetadata.year;
-                                }));
-                    console.log(
-                        "test1 corresponding ids: ",
-                        entities.map(function(e) {
-                          return e.id;
-                        }));
-                  });
-            });
-      }
-      test1();
+//calling testServerQuery
+function testServerQuery(secondMethodName)
+{
+	var dataset = "Obesity.csv";
+	var rRoutine = "obesityCSVRoutine.R";
+	queryRService(
+	//method
+	'runScriptOnCSVOnServer',
+	{
+		//params
+		queryObject:[dataset,rRoutine],
+		//queryStatement: "PercentObese2002,PercentObese2004",
+		//schema: "us"
+		
+	},
+	//callback
+	handleRResult
+	);
+	
+	function handleRResult(response)
+	{
+		if(response.error)
+		{
+			//resulttextarea.value = JSON.stringify(response,null,3);
+			return;
+		}
+		else
+		{
+			var rResult = response.result;
+			console.log("calling set CSVSource");
+			setCSVSource(rResult);
+			//console.log("retrieved " + rResult.length + "results");
+			//resulttextarea.value = "Success";
+			return;
+		}
+		
+		
+	}
+	
+	
+	function setCSVSource(rResult)
+	{
+		console.log(rResult[0].value);
+		var weave = document.getElementById('weave');
+		weave.path('MyDataSource').request('CSVDataSource').vars({data: rResult[0].value}).exec('setCSVDataString(data)');
+		weave.requestObject(['SPT'], 'ScatterPlotTool');
+		
+		/**
+		columnPath path of the object in session state in Weave
+		csvDataSource the source to use column from
+		columnNumber columnIndex or columnName in the csvDatasource
+		*/
+		weave.setCSVColumn = function(columnPath, csvDataSource, columnNumber) {
+		
+				//weave
+				//.path(csvDataSource)
+				//.vars({i: columnNumber, p: columnPath})
+				//.exec('putColumn(i, p)');
+				
+				var x = weave.path(csvDataSource);//gets the path object
+				x.vars({i:columnNumber, p:columnPath});//adds the required variables and attributes them
+				var y = x.exec('putColumn(i,p)');//calling funtion putColumn from CSVDataSource.as in Weave 
+		};
+		
+		weave.setCSVColumn(['SPT','children','visualization','plotManager','plotters','plot','dataX'],'MyDataSource', 'PercentObese2002');
+		weave.setCSVColumn(['SPT','children','visualization','plotManager','plotters','plot','dataY'],'MyDataSource', 'PercentObese2004');
+		
+		//can also specify the columnIndex
+		//weave.setCSVColumn(['SPT','children','visualization','plotManager','plotters','plot','dataY'],'MyDataSource', 'PercentObese2004');
+		
+		//setting the color column, columnIndex is hardcoded
+		weave.setCSVColumn(['defaultColorDataColumn', 'internalDynamicColumn'],'MyDataSource', 0);
+		
+		
+		
+		
+		//var scatterPlotX = weave.path("SPT","children","undefinedX","plotManager","plotters","plot","dataX","internalObject","dynamicColumnReference","internalObject");
+		//scatterPlotX.state("hierarchyPath", '<hierarchy><attribute keyType="null" csvColumn="PercentObese2002" title="PercentObese2002"/></hierarchy>');
+		
+		//var scatterPlotX = weave.path("SPT","children","undefinedXY","plotManager","plotters","plot");
+		//scatterPlotX.state = ("dataX", '<ReferencedColumn><dynamicColumnReference encoding="dynamic"> <HierarchyColumnReference><dataSourceName>MyDataSource</dataSourceName><hierarchyPath encoding="xml"><hierarchy><attribute csvColumn="PercentObese2002" keyType="null" title="PercentObese2002"/></hierarchy></hierarchyPath></HierarchyColumnReference></dynamicColumnReference></ReferencedColumn>');
+		
+		//var scatterPlotY = weave.path("SPT","children","undefinedXY","plotManager","plotters","plot");
+		//scatterPlotY.state = ("dataY", '<ReferencedColumn><dynamicColumnReference encoding="dynamic"> <HierarchyColumnReference><dataSourceName>MyDataSource</dataSourceName><hierarchyPath encoding="xml"><hierarchy><attribute csvColumn="PercentObese2004" keyType="null" title="PercentObese2004"/></hierarchy></hierarchyPath></HierarchyColumnReference></dynamicColumnReference></ReferencedColumn>');
+		//var scatterPlotY = weave.path("SPT", "children", "undefinedY", "plotManager", "plotters", "plot", "dataY", "internalObject", "dynamicColumnReference", "internalObject");
+		//scatterPlotY.state("hierarchyPath", '<hierarchy> <attribute keyType="null" csvColumn="PercentObese2004" title="PercentObese2004"/></hierarchy>');
+		
+		
 
-    });
+	}
+	
+	
+}
+
+});
