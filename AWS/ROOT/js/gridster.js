70ae57d66da26331293a07fa016166a76870618a
Franck Kamayou <franckamayou@gmail.com>
2013-07-03 13:22:19 -0400

    added communication to AdminService and DataService.

--- AWS/ROOT/js/gridster.js
+++ AWS/ROOT/js/gridster.js
@@ -36,13 +36,12 @@ require(
                   modal : true,
                   buttons : {
                     Select : function() {
-                      // handleSelectOption();
                       $(this).dialog("close");
                     },
                     Cancel : function() {
                       // handleCancelOption();
                       $(this).dialog("close");
-                    },
+                    }
                   }
                 });
 
@@ -51,13 +50,22 @@ require(
                 });
                 
                 $('#scriptButton').button().click(function() {
-                  // enter run script here
-                	testServerQuery('getRResult');
+                  var scriptType = $('#scriptCombobox').val();
+                  if (scriptType == "Normalization.R")
+                  {
+                	  	scriptOneQuery('getScriptOneResult');
+                  }
+                  if (scriptType == "obesityCSVRoutine.R")
+                  {
+                	   testServerQuery('getRResult');
+                  }
+                  
+                  
                 });
                 
-                $('#callWeaveButton').button().click(function() {
+                $('#scriptButton2').button().click(function() {
                   // enter function here for calling weave
-                	scriptOneQuery('getScriptOneResult');
+//                	scriptOneQuery('getScriptOneResult');
                 });
 
                 $('.portlet')
@@ -88,17 +96,11 @@ require(
                     
                 var scriptCombobox = $('#scriptCombobox');
                 
-                scriptCombobox.append($("<option/>").val('script1').text(
-                          "Script 1"));
-                scriptCombobox.append($("<option/>").val('script2')
-                          .text("Script 2"));
+                scriptCombobox.append($("<option/>").val('Normalization.R').text(
+                          "Normalization.R"));
+                scriptCombobox.append($("<option/>").val('obesityCSVRoutine.R')
+                          .text("obesityCSVRoutine.R"));
                 
-                var dataCombobox = $('#dataCombobox');
-                
-                dataCombobox.append($("<option/>").val('script1').text(
-                          "Obesity.csv"));
-                dataCombobox.append($("<option/>").val('script2')
-                          .text("Cars.csv"));
                 
                 var dialogOptions = {
                         "autoOpen": false,
@@ -107,8 +109,8 @@ require(
                 		"modal" : false,
                 		buttons : {
                 			"Export" : function() {
-                				console.log("Save button clicked");
-                			},
+                				
+                			}
                 		
                 		}
                 };
@@ -130,6 +132,14 @@ require(
                 
                 });
                 
+                $('#getHierarchy').button()
+                				  .click(function() {getDataHierarchy();});
+                				  
+                $('#dataTables').change(function() {
+                	getDataColumnIds($(this).val());
+                	
+                });
+                
               }));
 
               
@@ -138,10 +148,138 @@ $.ajaxSetup({
     cache: false
 });
 
+function getDataColumnIds(id) {
+	
+	queryDataService(
+		"getEntityChildIds", // method
+		[id],
+		handleColumnIdsResults
+	);
+}
+
+function getDataColumnEntities(ids) {
+	queryDataService(
+		"getEntitiesById",
+		[ids],
+		handleEntitiesResults
+	
+	);	
+
+}
+
+
+function handleColumnIdsResults(response) {
+	if(response.error)
+		{
+			alert("Error");
+			return;
+		}
+		else
+		{
+			//console.log(response.result);
+			getDataColumnEntities(response.result);
+			return;
+		}
+}
+
+function handleEntitiesResults(response) {
+	var dataColumns = [];
+	if(response.error)
+		{
+			alert("Error");
+			return;
+		}
+		else
+		{
+			$("#columns").empty();
+			$("#dataCombobox").empty();
+			dataColumns = [];
+			for (var i in response.result) {
+				var column = response.result[i];
+				dataColumns.push(column);
+				$("#columns").append($("<option/>").val(column.id.toString())
+							.text(column.publicMetadata.title));
+				$("#dataCombobox").append($("<option/>").val(column.id.toString())
+				.text(column.publicMetadata.title));
+			}
+			populatePanels(dataColumns);
+			return;
+		}
+}
+
+
+function getDataHierarchy() {
+	$('#scriptResults').html("getDataHierarchy Called");
+
+	queryAdminService(
+	   "getEntityHierarchyInfo", //method
+	   [$('input[name="ConnectionName"]').val(), $('input[name="Password2"]').val(), 0], //params
+	   handleDatabaseResult //callback
+	);
+	
+}
+
+function handleDatabaseResult(response){
+	if(response.error)
+		alert("connection failed");
+	else
+	{
+		for (var i in response.result)
+		{
+			var table = response.result[i];
+			$('#dataTables').append($("<option/>").val(table.id.toString())
+						.text(table.title + " (" + table.numChildren + ")"));
+		}
+		
+	}
+	return;
+}
+
+
+function populatePanels(dataColumns) {
+	// this function update panels with metadata from database
+	console.log(dataColumns);
+	for ( var i = 1; i < 7; i++)
+	{
+		$('#panel'+i).html(dataColumns[i-1].publicMetadata.title);
+	}
+	
+	return;
+	
+}
+
+function queryAdminService(method, params, callback)
+{
+	var url = '/WeaveServices/AdminService';
+	var request = {
+	               jsonrpc:"2.0",
+	               id:"no_id",
+	               method : method,
+	               params : params
+	};
+	
+	$.post(url, JSON.stringify(request), callback, "json");
+}
+
+function queryDataService(method, params, callback)
+{
+	var url = '/WeaveServices/DataService';
+	var request = {
+	               jsonrpc:"2.0",
+	               id:"no_id",
+	               method : method,
+	               params : params
+	};
+	
+	$.post(url, JSON.stringify(request), callback, "json");
+}
+
+
+
 //calling Rservice on Weave
 function queryRService(method,params,callback,queryID)
 {
-	$('#scriptResults').html("<p>" + JSON.stringify('queryRService',method,params));
+	console.log('queryRService',method,params);
 	var url = '/WeaveServices/RService';
 	var request = {
 					jsonrpc:"2.0",
@@ -149,8 +287,9 @@ function queryRService(method,params,callback,queryID)
 					method : method,
 					params : params
 	};
-	
-	$.post(url,JSON.stringify(request), callback, "json");
+	console.log(JSON.stringify(request));
+	var check = '"' + JSON.stringify(request) + '"' ;
+	$.post(url,request, callback, "json");
 	//resulttextarea.value = 'Awaiting Response for ' + method + ' request....';
 }
 
@@ -158,15 +297,33 @@ function queryRService(method,params,callback,queryID)
 
 function scriptOneQuery(secondMethodName)
 {
-	var dataset = "obesity.csv";
+	var datasetName = "Obesity.csv";
 	var rRoutine = "Normalization.R";
 	
+	
+	var jsqueryObject = new Object();
+	jsqueryObject.dataset = datasetName;
+	jsqueryObject.rRoutine = rRoutine;
+	jsqueryObject.age = 50;
+	//these are the columns which will be pulled out from the database
+	jsqueryObject.columnsToBeRetrieved = ["PercentObese2002", "PercentObese2004"];
+	
+	
+	var jsConnectionInfo = new Object();
+	jsConnectionInfo.user = "root";
+	jsConnectionInfo.password = "Tc1Sgp7nFc";//hard coded for now 
+	jsConnectionInfo.dbname = "resd";
+	jsConnectionInfo.schema = "us";
+		
 	queryRService(
 	//method
-	'runScriptOnCSVOnServer',
+	'manageComputation',
 	{
 		//params
-		queryObject:[dataset,rRoutine],
+		queryObject : jsqueryObject,
+		connectionInfoObject : jsConnectionInfo
+		//works
+		//queryObject:[dataset,rRoutine],
 		
 	},
 	//callback
@@ -177,15 +334,15 @@ function scriptOneQuery(secondMethodName)
 	{
 		if(response.error)
 		{
-			$('#scriptResults').html(JSON.stringify(response,null,3));
+			//resulttextarea.value = JSON.stringify(response,null,3);
 			return;
 		}
 		else
 		{
 			var rResult = response.result;
-			$('#scriptResults').html(JSON.stringify(rResult));
+			console.log("calling set CSVSource");
 			setCSV2Source(rResult);
-			$('#scriptResults').html("retrieved " + rResult.length + "results");
+			//console.log("retrieved " + rResult.length + "results");
 			return;
 		}
 		
@@ -195,15 +352,69 @@ function scriptOneQuery(secondMethodName)
 	
 	function setCSV2Source(rResult)
 	{
-		$('#scriptResults').html(rResult[0].value);
+		
+		console.log(rResult[0].value);
 		var weave = document.getElementById('weave');
 		weave.path('MyNORMDataSource').request('CSVDataSource').vars({data: rResult[0].value}).exec('setCSVDataString(data)');
-		weave.requestObject(['DTT'], 'DataTableTool');
+		
+		/*setting up a maptool
+		setting the geometry collections from a WeaveDataSource
+		setting the color column of the map tool from the results returned by the computation
+		*/
+		weave.requestObject(['Maptool1'], 'MapTool');
+		var mapOne = weave.path.apply(weave,[]).push('worldborders');
+		var mapTwo = mapOne.request('ReferencedColumn').push('dynamicColumnReference',null);
+		var mapThree = mapTwo.request('HierarchyColumnReference').state('dataSourceName','WeaveDataSource').
+						state('hierarchyPath','attribute weaveEntityId ="152" name ="world" title="world" dataType = "geometry"/>').pop().pop();
 		
 		
 		
+		//setting up datatable tool
+		
+		//weave.requestObject(['DTT'], 'DataTableTool');
+		/*Method 1*/
+		//var one = weave.path.apply(weave,['DTT','columns']).push('x');
+		//var two = one.request('ReferencedColumn').push('dynamicColumnReference',null);			
+		//var three = two.request('HierarchyColumnReference')
+		//			.state('dataSourceName','MyNORMDataSource')
+					//.state('hierarchyPath', '<attribute keyType="CSVDataSource" title="PercentObese2002" csvColumn="PercentObese2002"/>')
+					//.pop()
+					//.pop();
+					
+		//compare with the next block of code
+		
+		//weave.setColumn = function(path, csvDataSource)
+		//{
+				//weave.path.apply(weave,path)
+					//.push(null)//should not be null
+					//.request('ReferencedColumn')
+					//.push('dynamicColumnReference',null)//null works only with linkableDynamicObject
+					//.request('HierarchyColumnReference')
+					//.state('dataSourceName', csvDataSource)
+					//.state('hierarchyPath', '<attribute keyType="CSVDataSource" title="PercentObese2002" csvColumn="PercentObese2002"/>')
+					//.pop()
+					//.pop();
+		//}
+		
+		
+		
+		/*Method 2*/
+		//weave.setCSVColumn = function(columnPath, csvDataSource, columnNumber) {
+		
+				//var x = weave.path(csvDataSource);//gets the path object
+				//x.vars({i:columnNumber, p:columnPath});//adds the required variables and attributes them
+				//var y = x.exec('putColumn(i,p)');//calling funtion putColumn from CSVDataSource.as in Weave 
+		//};
+		//weave.setCSVColumn(['DTT','columns','x'],'MyNORMDataSource','PercentObese2002');
+		//weave.setCSVColumn(['DTT','columns','y'],'MyNORMDataSource','PercentObese2004');
 	}
+		
+		
 }
+//---------------------------------called by scriptOneButton END---------------------------
+
+
+
 //---------------------------------called by scriptOneButton END---------------------------
 
 
@@ -218,7 +429,7 @@ function testServerQuery(secondMethodName)
 	'runScriptOnCSVOnServer',
 	{
 		//params
-		queryObject:[dataset,rRoutine],
+		queryObject:[dataset,rRoutine]
 		//queryStatement: "PercentObese2002,PercentObese2004",
 		//schema: "us"
 		
@@ -236,10 +447,8 @@ function testServerQuery(secondMethodName)
 		}
 		else
 		{
-			$('#resultScripts').html("Script succeeded");
 			var rResult = response.result;
-			$('#resultScripts').html("calling set CSVSource");
-			$('#resultScripts').html(JSON.stringify(rResult));
+			console.log("calling set CSVSource");
 			setCSVSource(rResult);
 			//console.log("retrieved " + rResult.length + "results");
 			//resulttextarea.value = "Success";
@@ -252,7 +461,7 @@ function testServerQuery(secondMethodName)
 	
 	function setCSVSource(rResult)
 	{
-		$('#resultScripts').html(rResult[0].value);
+		console.log(rResult[0].value);
 		var weave = document.getElementById('weave');
 		weave.path('MyDataSource').request('CSVDataSource').vars({data: rResult[0].value}).exec('setCSVDataString(data)');
 		weave.requestObject(['SPT'], 'ScatterPlotTool');
@@ -281,7 +490,7 @@ function testServerQuery(secondMethodName)
 		//weave.setCSVColumn(['SPT','children','visualization','plotManager','plotters','plot','dataY'],'MyDataSource', 'PercentObese2004');
 		
 		//setting the color column, columnIndex is hardcoded
-		weave.setCSVColumn(['defaultColorDataColumn', 'internalDynamicColumn'],'MyDataSource', 0);
+		weave.setCSVColumn(['defaultColorDataColumn', 'internalDynamicColumn'],'MyDataSource', 'PercentObese2005');
 		
 		
 		
