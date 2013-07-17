78bebd18a81e0bf80137875b0decd0c8e8605f71
Franck Kamayou <franckamayou@gmail.com>
2013-07-17 13:37:29 -0400

    deleting unnecessary folder, and working on the Data Dialog...

--- AWS/app/js/controllers/dataDialogCtrl.js
+++ AWS/app/js/controllers/dataDialogCtrl.js
@@ -3,7 +3,7 @@
  * DataDialogCtrl - Controls dialog button and closure.
  * DataDialogConnectCtrl - Manages the content of the Dialog. 
  */
-angular.module('aws.DataDialog', [])
+angular.module('aws.DataDialog', ['aws'])
 .controller('DataDialogCtrl',function($scope, $dialog){
   $scope.opts = {
     backdrop: true,
@@ -13,45 +13,28 @@ angular.module('aws.DataDialog', [])
     controller: 'DataDialogConnectCtrl'
   };
 
-  $scope.openDialog = function(partial){
-  	if(partial){
+$scope.openDialog = function(partial){
+  	
+	if(partial){
 		$scope.opts.templateUrl = 'tlps/' + partial + '.tlps.html';
 	}
+	
 	var d = $dialog.dialog($scope.opts);
-    d.open().then(function(result){
-      if(result)
-      {
-        alert('dialog closed with result: ' + result);
-      }
-    });
+    
+	d.open().then(aws.DataClient.getDataTableList(function(result){console.log(result)}));
+	
+	$scope.dataTables = [];
+	$scope.dataColumns = [];
+	
   };
 })
+
 .controller('DataDialogConnectCtrl', function($scope, $http, dialog){
 
-	$scope.dataTables = null;
-	$scope.conn = {
-		serverType: "",
-		sqlip:  "demo.oicweave.org",
-		sqlport: "3306",
-		sqluser: "root",
-		sqlpass: "Tc1Sgp7nFc",
-		sqldbname: "",
-		connectionName: "",
-		connectionPass: ""
-	};
-
-	$scope.close = function(result){
-	    dialog.close(result);
+	
+	
+	$scope.close = function(){
+	    dialog.close();
 	 };
 
-	$scope.connect = function(){
-		console.log("entered connect fcn");
-		var res = $scope.getEntityHierarchyInfo($scope.conn.connectionName, $scope.conn.connectionPass, 0);
-		for(var i in res){
-			var table = res[i];
-			$('#dataTables').append($("<option/>").val(table.id.toString()).text(table.title + " (" + table.numChildren + ")"));
-			//appending this table of results to the data table selector
-		}
-	};
-	
 })
