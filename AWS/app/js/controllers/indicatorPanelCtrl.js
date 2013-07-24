c5c1f3589c07703ce01defd5791b4bee7531a0af
patchcr <patchcr@gmail.com>
2013-07-24 18:14:45 -0400

    Column Names from DataServices populate the selector panels.
    Conflicts:
    	AWS/app/js/controllers/indicatorPanelCtrl.js
    	AWS/app/js/services/services.js

--- AWS/app/js/controllers/indicatorPanelCtrl.js
+++ AWS/app/js/controllers/indicatorPanelCtrl.js
@@ -1,17 +1,16 @@
 angular.module("aws.IndicatorPanel", [])
-.controller("IndicatorPanelCtrl", function($scope, queryobj){
+.controller("IndicatorPanelCtrl", function($scope, queryobj, Data){
 	// Need to add code that goes to get the options from the server. 
-	
-	$scope.options = ["one", "two", "three"]; // = $scope.getIndicatorsFromDb()
+
+	var getPromise = Data.getColNamesFromDb("indicator", $scope);
+	var answer = ["one", "two", "three"];
+	$scope.options = getPromise;
+
+	//$scope.options = answer;
 	$scope.selection;
 	
 	$scope.$watch('selection', function(){
 		queryobj[$scope.panelTitle] = $scope.selection;
 	});
-	
-	$scope.getIndicatorsFromDb = function(){
-		// aws.DataClient.getIndicators()??
-	};
-	
 
 })
--- AWS/app/js/services/services.js
+++ AWS/app/js/services/services.js
@@ -25,8 +25,84 @@ angular.module('aws.services', [ 'ngResource' ]).factory('Phone',
  */
 angular.module("aws").service("queryobj", function() {
 	this.queryObject = {
-		title : "starcraft"
+		connectionDB : "exampleProperty"
 	};
-	this.selectedColumns = [];
 
+})
+
+angular.module("aws").service("Data", function($q, $timeout){
+
+	return {
+		getColNamesFromDb: function(panel, scope){
+			// aws.DataClient.getIndicators()??
+			var deferred = $q.defer();
+			var prom = deferred.promise;
+			var deferred2 = $q.defer();
+//			var intermediate;
+//	//		var childIds = deferred.promise;
+//			var out = function(result){
+//				console.log("out handler");
+//				console.log(result);
+//				//deferred.promise.result = result;
+//				if(result){
+//					deferred.resolve(result);
+//					//deferred.reject();
+//				}else{
+//					
+//				}
+//				//$rootScope.$apply();
+//			};
+			
+			function safeApply( fn ) {
+                if ( !scope.$$phase ) {
+                    scope.$apply( fn );
+                }
+                else {
+                    fn();
+                }
+            }
+			var callbk = function(result){
+				safeApply(function(){
+					console.log(result);
+					deferred.resolve(result);
+				});
+			};
+			var callbk2 = function(result){
+				safeApply(function(){
+					console.log(result);
+					deferred2.resolve(result);
+				});
+			};
+//			var safeApply = function(scope, fn) {
+//			    (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+//			};
+			
+				aws.DataClient.getEntityChildIds(161213, callbk);
+
+				deferred.promise.then(function(res){
+					aws.DataClient.getDataColumnEntities([res], callbk2);
+				});
+				prom = deferred2.promise.then(function(response){
+					console.log(response);
+					//$scope.options = response;
+					return response;
+				},function(response){
+					console.log("error " + response);
+				});
+			
+			//deferred2.resolve(aws.DataClient.getDataColumnEntities(childIds));
+	//		var temp = deferred.promise.then(function(result){
+	//			return result;
+	//		});
+//			deferred.promise.then(function(response){
+//				deferred.resolve(intermediate);
+//			});
+			//deferred.promise.result = intermediate;
+			return prom;
+		},
+		secondMethod: function(bogus){
+			console.log("bogus");
+			return "blah";
+		}
+	};
 });
