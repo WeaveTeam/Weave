7db389e5ed8b2a4505293ed72a6e03e74add7adf
jtfallon <John_Fallon@student.uml.edu>
2013-07-25 16:20:12 -0400

    Fixed DataDialog sizing. The ProjectMenu dialog box now appears, but needs to be populated still. I have added a javascript library that should allow us to save files to a user's computer.

--- AWS/app/js/controllers/projectCtrl.js
+++ AWS/app/js/controllers/projectCtrl.js
@@ -1,19 +1,30 @@
 /**
- * Project Module
- * ProjectButtonCtrl - Controls actions of the project button.
+ * Project Module ProjectButtonCtrl - Controls actions of the project button.
  * ProjectContentCtrl - Controls dialog content for project actions.
  */
-angular.module('aws.project', [])
-.controller('ProjectButtonCtrl', function(){
+angular.module('aws.project', [ 'aws' ]).controller('ProjectButtonCtrl',
+		function($scope, $dialog) {
+			$scope.opts = {
+				backdrop : true,
+				keyboard : true,
+				backdropClick : true,
+				templateUrl : 'tlps/ProjectMenu.tlps.html',
+				controller : 'ProjectButtonCtrl'
+			};
 
-	$scope.opts = {
-	    templateUrl: 'tlps/ProjectMenu.tlps.html'
-	}
-	
-	$scope.items = [
-	  "New...",
-	  "Open",
-	  "Save",
-	  "Quit"
-	];
+			$scope.openDialog = function(partial) {
+				console.log("hello");
+				if (partial) {
+					$scope.opts.templateUrl = 'tlps/' + partial + '.tlps.html';
+				}
+
+				var d = $dialog.dialog($scope.opts);
+				d.open();
+			};
+		})
+
+.controller('ProjectButtonCtrl', function($scope, $http, dialog) {
+	$scope.close = function() {
+		dialog.close();
+	};
 })
