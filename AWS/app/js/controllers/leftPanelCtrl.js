136c420f53efa1f4053b4dc769fd5e17fb22050e
jtfallon <John_Fallon@student.uml.edu>
2013-07-30 15:29:23 -0400

    Added in ability to import a JSON query file and then execute it. I am unable to test it, so someone will need to test it with a legitimate query.

--- AWS/app/js/controllers/leftPanelCtrl.js
+++ AWS/app/js/controllers/leftPanelCtrl.js
@@ -1,16 +1,17 @@
 /**
- * Left Panel Module
- * LeftPanelCtrl - Manages the model for the left panel.
+ * Left Panel Module LeftPanelCtrl - Manages the model for the left panel.
  */
-angular.module("aws.leftPanel", [])
-.controller("LeftPanelCtrl", function($scope, $location){
-	$scope.isActive = function(route){
-		return route == $location.path();
-	};
-	
-});
+angular.module("aws.leftPanel", []).controller("LeftPanelCtrl",
+		function($scope, $location) {
+			$scope.isActive = function(route) {
+				return route == $location.path();
+			};
+
+		});
 
 function saveJSON(query) {
-	var blob = new Blob([JSON.stringify(query, undefined, 2)], {type: "text/plain;charset=utf-8"});
+	var blob = new Blob([ JSON.stringify(query, undefined, 2) ], {
+		type : "text/plain;charset=utf-8"
+	});
 	saveAs(blob, "Query Object.txt");
 }
