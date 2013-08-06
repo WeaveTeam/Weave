eff173b0877d6b2d722f4521c3aa9e3f08aafc6e
Franck Kamayou <franckamayou@gmail.com>
2013-08-06 17:00:42 -0400

    changes to WeavePathExterns

--- AWS/externs/WeavePathExterns.js
+++ AWS/externs/WeavePathExterns.js
@@ -5,18 +5,36 @@
  * @externs
  */
 
- 
+/**
+ * @typedef {Array.<string>}
+ */
+var Path;
+
 /**
  * @constructor
- * @param {Array.<string>} path The base path Array
+ * @param {Path} path The base path Array
  *
  * @return {!WeavePath}
  */
 function WeavePath(path) {}
 
 /**
- * Returns a copy of the current path Array.
- * Accepts an optional list of names to be appended to the result.
+ * This function returns a copy of the current path Array.
+ * @param {Path=} relativePath optional list of names to be appended to the result.
+ * @return {Path}
+ */
+function getPath(relativePath) {};
+
+/**
+ * Gets an Array of child names under the object at the current path or relative to the current path.
+ * @param {Path=} relativePath an optional list of names to be appended to the result.
  * @return {Array.<string>}
  */
-function getPath() {};
+function getNames(relativePath) {};
+
+/**
+ * Gets the type (qualified class name) of the object at the current path or relative to the current path.
+ * @param {Path=} relativePath an optional list of names relative to the current path.
+ * @return {string}
+ */
+function getType(relativePath) {};
