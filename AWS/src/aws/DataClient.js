1b8e3c5fa36b09be350cb9c23883d335f17d876f
adufilie <andy.dufilie@gmail.com>
2013-12-10 22:09:07 -0500

    Added aws.DataClient.getDataMapping().
    Change-Id: Ia33ddcd1432b55b6a0ab39039185c1a6327f09e4

--- AWS/src/aws/DataClient.js
+++ AWS/src/aws/DataClient.js
@@ -12,7 +12,6 @@ var dataServiceURL = '/WeaveServices/DataService';
 aws.DataClient.getDataTableList = function(handleResult) {
 	aws.queryService(dataServiceURL, "getDataTableList", null, handleResult);
 };
-
 	
 /**
  * This function mirrors the getEntityChildIds on the servlet.
@@ -47,8 +46,43 @@ aws.DataClient.getColumn = function(columnId, minParam, maxParam, sqlParams, han
 	aws.queryService(dataServiceURL, "getColumn", [columnId, minParam, maxParam, sqlParams], handleResult);	
 };
 
-
+/**
+ * @param {Object} meta
+ * @param {function(Object)} handleResult
+ */
 aws.DataClient.getEntityIdsByMetadata = function(meta, handleResult){
 	// Assuming we want a column back, the dataEntity Type should be 1.
 	aws.queryService(dataServiceURL, "getEntityIdsByMetadata", [meta, 1], handleResult);
+};
+
+/**
+ * Accepts a varValues property from an aws_metadata object and passes the corresponding data mapping to a callback function.
+ * @param {Object} varValues Either an Array of value-label pairs or a column identifier in one of three formats:
+ *   1. An integer for a column entity id (used by the admin console);
+ *   2. An Object containing metadata fields that uniquely identify the column; and
+ *   3. A String which specifies the value for a single predefined metadata field (aws_id) which uniquely identifies a column.
+ * @param {function(Array.<Object>)} A callback which receives the data mapping.
+ */
+aws.DataClient.getDataMapping = function(varValues, callback)
+{
+	if (Array.isArray(varValues))
+	{
+		setTimeout(function(){ callback(varValues); }, 0);
+		return;
+	}
+	
+	if (typeof varValues == 'string')
+		varValues = {"aws_id": varValues};
+	
+	aws.queryService(
+		dataServiceURL,
+		'getColumn',
+		[varValues, NaN, NaN, null],
+		function(columnData) {
+			var result = [];
+			for (var i in columnData.keys)
+				result[i] = {"value": columnData.keys[i], "label": columnData.data[i]};
+			callback(result);
+		}
+	);
 };
