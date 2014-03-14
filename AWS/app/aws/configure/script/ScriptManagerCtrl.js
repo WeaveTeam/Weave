51d6e92a0e606878570410cac35a725c6698fda0
patrick <patchcr@gmail.com>
2014-03-14 16:31:02 -0400

    clean up of some project stuff. Added a "simple" build target that won't recompile the closure code. Script metadata progress.

--- AWS/app/aws/configure/script/ScriptManagerCtrl.js
+++ AWS/app/aws/configure/script/ScriptManagerCtrl.js
@@ -1,42 +1,48 @@
-angular.module('aws.configure.script', [])
-        .controller("ScriptManagerCtrl", function($scope, scriptManagerService) {
+angular.module('aws.configure.script', ['ngGrid'])
+  .controller("ScriptManagerCtrl", function($scope, scriptManagerService) {
 
-          $scope.listOfScripts = [];
-          $scope.uploadScript = false;
-          $scope.textScript = false;
-          $scope.saveButton = false;
-          scriptManagerService.getListOfScripts();
-          $scope.selectedScript = "";
-          $scope.selectedMetadata = {};
+    $scope.listOfScripts = [];
+    $scope.uploadScript = false;
+    $scope.textScript = false;
+    $scope.saveButton = false;
+    scriptManagerService.getListOfScripts();
+    $scope.selectedScript = [];
+    $scope.selectedMetadata = {};
 
-          $scope.$watch(function() {
-            return scriptManagerService.dataObject.listOfScripts;
-          }, function() {
-            $scope.listOfScripts = [];
-            angular.forEach(scriptManagerService.dataObject.listOfScripts, function(item) {
-              $scope.listOfScripts.push({name: item});
-            });
+    $scope.$watch(function() {
+      return scriptManagerService.dataObject.listOfScripts;
+    }, function() {
+      $scope.listOfScripts = [];
+      angular.forEach(scriptManagerService.dataObject.listOfScripts, function(item) {
+        $scope.listOfScripts.push({name: item});
+      });
+    });
+    $scope.$watch(function(){
+      return scriptManagerService.dataObject.scriptMetadata;
+    },function(nv){
+      $scope.selectedMetadata = nv;
+      console.log("metadata:", $scope.selectedMetadata);
+    });
+    $scope.gridOptions = {data: 'listOfScripts',
+      columnDefs: [{field: 'name', displayName: 'Name'}],
+      selectedItems: $scope.selectedScript,
+      multiSelect: false,
+      afterSelectionChange: function(item){
+        console.log("selectionchange", item);
+        scriptManagerService.getScriptMetadata($scope.selectedScript[0].name);
+      }
+    };
+    $scope.showUpload = function() {
+      console.log("clicked");
+      $scope.uploadScript = true;
+      $scope.textScript = false;
+      $scope.saveButton = true;
+    };
+    $scope.showTextArea = function() {
+      $scope.uploadScript = false;
+      $scope.textScript = true;
+      $scope.saveButton = true;
+    };
 
-          });
-          $scope.$watch("selectedScript",function(nv, ov){
-            $scope.selectedMetadata = scriptManagerService.getScriptMetadata($scope.selectedScript);
-          });
-          $scope.gridOptions = {data: 'listOfScripts',
-            columnDefs: [{field: 'name', displayName: 'Name'}],
-            selectedItems: $scope.selectedScript,
-            multiSelect: false
-          };
-          $scope.showUpload = function() {
-            console.log("clicked");
-            $scope.uploadScript = true;
-            $scope.textScript = false;
-            $scope.saveButton = true;
-          };
-          $scope.showTextArea = function() {
-            $scope.uploadScript = false;
-            $scope.textScript = true;
-            $scope.saveButton = true;
-          };
-
-        });
+  });
 			
