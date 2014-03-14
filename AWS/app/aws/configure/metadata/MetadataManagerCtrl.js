4e43c092065933e936b6f93f4f8a8fc7dd1938d2
Franck Kamayou <franckamayou@gmail.com>
2014-03-14 10:33:22 -0400

    now able to edit and add single metadata value

--- AWS/app/aws/configure/metadata/MetadataManagerCtrl.js
+++ AWS/app/aws/configure/metadata/MetadataManagerCtrl.js
@@ -1,7 +1,11 @@
 angular.module('aws.configure.metadata', []).controller("MetadataManagerCtrl", function($scope, queryService){			
 	
 	var treeData = [];
-
+	$scope.myData = [];
+	$scope.maxTasks = 100;
+	$scope.progressValue = 0;
+	$scope.selectedColumnId;
+	
 	var generateTree = function() {
 		queryService.getDataTableList().then(function(dataTableList) {
 			for (var i = 0; i < dataTableList.length; i++) {
@@ -23,10 +27,12 @@ angular.module('aws.configure.metadata', []).controller("MetadataManagerCtrl", f
 							$("#tree").dynatree({
 								minExpandLevel: 1,
 								children : treeData,
+								keyBoard : true,
 								onPostInit: function(isReloading, isError) {
 									this.reactivate();
 								},
 								onActivate: function(node) {
+									$scope.selectedColumnId = node.data.key;
 									getColumnMetadata(node.data.key);
 								},
 								debugLevel: 0
@@ -39,140 +45,124 @@ angular.module('aws.configure.metadata', []).controller("MetadataManagerCtrl", f
 	};
 	
 	generateTree();
-	
+
 	var getColumnMetadata = function (id) {
-		
 		aws.DataClient.getDataColumnEntities(id, function(result) {
 			var metadata = result[0];
 			if(metadata.hasOwnProperty("publicMetadata")) {
 				if(metadata.publicMetadata.hasOwnProperty("aws_metadata")) {
 					var data = [];
 					var aws_metadata = angular.fromJson(metadata.publicMetadata.aws_metadata);
-					for (key in aws_metadata) {
-						data.push({property : key, value : aws_metadata[key] });
-					}
+					data = convertToTableFormat(aws_metadata);
 					setMyData(data);
+				} else {
+					setMyData([]);
 				}
-			}
+			} 
 		});
 	};
-	 $scope.myData = [{property:'property1', value: 'value1'}];
-	 
+	
+	var convertToTableFormat = function(aws_metadata) {
+		var data = [];
+		for (var key in aws_metadata) {
+			data.push({property : key, value : aws_metadata[key] });
+		}
+		return data;
+	};
+	
+	var convertToMetadataFormat = function(tableData) {
+		var aws_metadata = {};
+		for (var i in tableData) {
+			aws_metadata[tableData[i].property] = tableData[i].value;
+		}
+		return aws_metadata;
+	};
+	
 	 var setMyData = function(data) {
-		 console.log($scope.myData);
 		  $scope.myData = data;
 		  $scope.$apply();
 	 };
+	 
 	 $scope.gridOptions = { 
 	        data: 'myData',
-	        enableCellSelection: true,
-	        enableRowSelection: false,
-	        enableCellEditOnFocus: true,
-	        columnDefs: [{field: 'property', displayName: 'Property', enableCellEdit: false}, 
-	                     {field:'value', displayName:'Value', enableCellEdit: true}]
+	        enableRowSelection: true,
+	        enableCellEdit: true,
+	        columnDefs: [{field: 'property', displayName: 'Property', enableCellEdit: true}, 
+	                     {field:'value', displayName:'Value', enableCellEdit: true}],
+	        multiSelect : false
+	                     
 	 };
-
-	 $scope.$watch(function() {
-			return queryService.dataObject.columns;
-		}, function(){
-			if(queryService.dataObject.columns) {
-				$scope.columnList = $.map(queryService.dataObject.columns, function(item){
-						return {
-							id : item.id,
-							title : item.publicMetadata.title,
-							aws_metadata : item.publicMetadata.aws_metadata
-						};
-				});
-			}					
-		});
 	 
-
-		$scope.$watch('dataTable', function() {
-			if($scope.dataTable){
-				queryService.getDataColumnsEntitiesFromId($scope.dataTable.id, true);
-				$('#log').html('');
-			};
-		});
+	 $scope.addNewRow = function () {
+		 $scope.myData.push({property: 'Property Name', value: 'Value'});
+	 };
+	  
+	 $scope.$on('ngGridEventEndCellEdit', function(){
+		 var jsonaws_metadata = angular.toJson(convertToMetadataFormat($scope.myData));
+		 queryService.updateEntity("mysql", 
+			"pass", 
+			$scope.selectedColumnId, { 
+										publicMetadata : { 
+															aws_metadata : jsonaws_metadata 
+														}
+										}
+		 ).then(function() {
+			 $scope.progressValue = 100;
+		 });
+	 });
+	 
+	$scope.$watch('columnSelected', function(){
+		if($scope.columnSelected){
+			$scope.aws_metadataTextArea = JSON.stringify(JSON.parse($scope.columnSelected.aws_metadata), null, 2);
+			$('#log').html('');
+		} else {
+			$scope.aws_metadataTextArea = "";
+		}
+	});
 		
-		$scope.$watch('columnSelected', function(){
-			if($scope.columnSelected){
-				$scope.aws_metadataTextArea = JSON.stringify(JSON.parse($scope.columnSelected.aws_metadata), null, 2);
-				$('#log').html('');
-			} else {
-				$scope.aws_metadataTextArea = "";
-			}
-		});
+	$scope.columnList = [];
 		
-		$scope.columnList = [];
+	$scope.refresh = function() {
+		generateTree();
+		$scope.$apply();
+	};
 		
-		$scope.showUpload = function() {
-			console.log("clicked");
-			$scope.uploadScript = true;
-			$scope.textScript = false;
-			$scope.saveButton = true;
-		};
-		$scope.showTextArea = function() {
-			$scope.uploadScript = false;
-			$scope.textScript = true;
-			$scope.saveButton = true;
-		};
-		
-
-		$scope.update = function() {
-			queryService.updateEntity("mysql", 
-										"pass", 
-										$scope.columnSelected.id, { 
-																	publicMetadata : { 
-																						aws_metadata : JSON.stringify(JSON.parse($scope.aws_metadataTextArea)) 
-																					}
-																	}
-									 ).then(function() {
-										$scope.refresh();
-										$('#log').html(" metadata updated...");
-									 });
-		}; 
-		
-		$scope.refresh = function() {
-			generateTree();
-		};
-		
-		$scope.updateFromCSV = function() {
-			var metadataArray = queryService.CSVToArray($scope.aws_metadataCSV);
+	$scope.updateFromCSV = function() {
 			
+			var metadataArray = queryService.CSVToArray($scope.aws_metadataCSV);
+			aws.DataClient.getEntityChildIds($scope.selectedColumnId, function() {
+				
+			});
 			if($scope.dataTable) {
 				for (var i = 1; i < metadataArray.length; i++) {
 					var metadata = metadataArray[i][1];
 					var title = metadataArray[i][0];
-					var progress = 0;
+					$scope.progressValue = 0;
 					var end = $scope.columnList.length;
 					var id = -1;
 					var j = 0;
 					for(j = 0; j < $scope.columnList.length; j++) {
 						if($scope.columnList[j].title == title) {
 							id = $scope.columnList[j].id;
-							break; // we assume there is only one match
-						}
+					 	break; // we assume there is only one match
 					}
-					
-					if(id != -1) {
-						queryService.updateEntity("mysql", 
-								"pass", 
-								 id, { 
-															publicMetadata : { 
-																				aws_metadata : metadata.replace(/\s/g, '')
-																			}
-															}
-							 ).then(function() {
-								 progress++;
-								 $('#log').html(progress);
-								 $('#log').append(" of ");
-								 $('#log').append(end);
-								 $('#log').append(" metadata updated...");
-							 });								
-					}
-				}						
-			} else {
-				$('#log').html("select a data table..");
-			}
-		};
+				}
+				
+				if(id != -1) {
+					queryService.updateEntity("mysql", 
+							"pass", 
+							 id, { 
+														publicMetadata : { 
+																			aws_metadata : metadata.replace(/\s/g, '')
+																		}
+														}
+						 ).then(function() {
+							 $scope.progressValue++;
+						 });								
+				}
+			}						
+		} else {
+			$('#log').html("select a data table..");
+		}
+	};
 });
