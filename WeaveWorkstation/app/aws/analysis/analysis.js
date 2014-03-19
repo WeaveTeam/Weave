d9d8148a63a70d075e3cf69ce0342c428bfe0a80
amithnair <amithbnair@gmail.com>
2014-03-19 00:51:02 -0400

    Add checkbox functionality and weave resources

--- WeaveWorkstation/app/aws/analysis/analysis.js
+++ WeaveWorkstation/app/aws/analysis/analysis.js
@@ -11,13 +11,20 @@ analysis_mod.controller('WidgetsController', function($scope, $filter, dasboard_
 	$scope.tool_list = dasboard_widget_service.get_tool_list('visualization');
 	$scope.filter_tools = dasboard_widget_service.get_tool_list('datafilter');
 
+	
+	
+	
 	$scope.add_widget = function(element_id) {
 		dasboard_widget_service.add_widget_bricks(element_id);
-	}
+	};
 
 	$scope.remove_widget = function(widget_index) {
 		dasboard_widget_service.remove_widget_bricks(widget_index);
-	}
+	};
+	
+	$scope.enable_widget = function(id, enabled){
+		dasboard_widget_service.enable_widget(id, enabled);
+	};
 	
 	
 });
