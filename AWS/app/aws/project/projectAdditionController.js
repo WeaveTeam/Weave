bc80e4a59227e9310d1cfd90b5f04bfc97a86682
Shweta <Shweta@129.63.17.163>
2014-03-19 15:16:40 -0400

    added files for project management interface

---
+++ AWS/app/aws/project/projectAdditionController.js
@@ -0,0 +1,8 @@
+/**
+ * This controller controls the queryObject (json ) Editor
+ */
+angular.module('aws.project', [])
+.controller("projectAdditionController", function($scope, queryService){
+	
+			
+});
