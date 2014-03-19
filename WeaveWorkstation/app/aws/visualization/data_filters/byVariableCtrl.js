a7c77add172df2b9d7ff08796c692ae872cec438
franckamayou <franckamayou@gmail.com>
2014-03-19 19:28:02 -0400

    by variable panel progress

--- WeaveWorkstation/app/aws/visualization/data_filters/byVariableCtrl.js
+++ WeaveWorkstation/app/aws/visualization/data_filters/byVariableCtrl.js
@@ -1,9 +1,14 @@
 analysis_mod.controller('byVariableCtrl', function($scope, queryService){
 	
+	queryService.queryObject.ByVariableFilter = [];
+	$scope.oldVal = [];
 	$scope.byVariableColumns = [];
 	$scope.byVariableSelection = [];
+	$scope.filterType = [];
+	$scope.filterValues = [];
+	$scope.filterOptions = [];
 	$scope.items = [1];
-	lastIndex = 1;
+	var lastIndex = 1;
 
 	$scope.$watch(function() {
 		return queryService.dataObject.columns;
@@ -20,42 +25,89 @@ analysis_mod.controller('byVariableCtrl', function($scope, queryService){
 							}
 						}
 					}
-				});
+			});
 		};
 	});
 	
-	$scope.$watchCollection('byVariableSelection', function() {
-		// loop over the selections
+	$scope.$watchCollection('byVariableSelection', function(newValue, oldValue) {
+	    // loop over the selections
 		for(var i in $scope.byVariableSelection) {
-			var column = angular.fromJson($scope.byVariableSelection[i]);
-			console.log(column);
-			var metadata;
-			// find the original column this came from using the id
-			for(var j in queryService.dataObject.columns) {
-				if(column.id == queryService.dataObject.columns[j]){
-					if( queryService.dataObject.columns[j].publicMetadata.hasOwnProperty("aws_metadata") ) {
-						metadata = angular.fromJson(queryService.dataObject.aws_metadata);
-						break;// break once we find match
+			if($scope.byVariableSelection[i] != "") {
+				var column = angular.fromJson($scope.byVariableSelection[i]);
+				var metadata;
+				// find the original column this came from using the id
+				for(var j in queryService.dataObject.columns) {
+					if(column.id == queryService.dataObject.columns[j].id){
+						if( queryService.dataObject.columns[j].publicMetadata.hasOwnProperty("aws_metadata") ) {
+							metadata = angular.fromJson(queryService.dataObject.columns[j].publicMetadata.aws_metadata);
+							break;// break once we find match
+						}
 					}
 				}
+				
+				queryService.queryObject.ByVariableFilter[i] = { 
+																	column : column
+															   }
+				if(metadata) {
+					if(metadata.hasOwnProperty("varType") && metadata.hasOwnProperty("varValues")) {
+						$scope.filterType[i] = metadata.varType;
+						$scope.filterOptions[i] = metadata.varValues;
+					} else {
+						$scope.filterType[i] = "";
+						$scope.filterOptions[i] = [];
+						$scope.filterValues[i] = "";
+					}
+				} else {
+					$scope.filterType[i] = "";
+					$scope.filterOptions[i] = [];
+					$scope.filterValues[i] = "";
+				}
+			} else {
+				queryService.queryObject.ByVariableFilter[i] = {};
 			}
-			
-			if(metadata) {
-				if(metadata.hasOwnProperty("varType") && metadata.hasOwnProperty("varValues")) {
-					$scope.filterType = metadata.varType;
-					$scope.filterValues[i] = metadata.varValues;
+		}
+	});
+	
+	$scope.$watchCollection('filterValues', function () {
+		for(var i in $scope.filterValues){
+			if ($scope.filterValues[i] != undefined && $scope.filterValues[i] != []) {
+
+				var temp = $.map($scope.filterValues[i], function(item) {
+					return angular.fromJson(item);
+				});
+
+				if ($scope.filterType[i] == "categorical") {
+					queryService.queryObject.FilteredColumnRequest[i].filters = { filterValues : temp };
+				} else if ($scope.filterType[i] == "continuous") {// continuous, we want arrays of ranges
+					queryService.queryObject.FilteredColumnRequest[i].filters = { filterValues : [temp]};
 				}
 			}
 		}
 	});
 	
+	$scope.$watchCollection(function() {
+		return queryService.queryObject.ByVariableFilter;
+	}, function () {
+		
+	});
+
 	$scope.addByVariable = function() {
-		$scope.items.push(lastIndex + 1);
+		lastIndex++;
+		$scope.items.push(lastIndex);
 	}
 	
 	$scope.removeByVariable = function(index) {
-		$scope.items.splice(index, 1);
-		$scope.byVariableSelection.splice(index, 1);
+		if($scope.items.length != 1) {
+			$scope.items.splice(index, 1);
+			$scope.byVariableSelection.splice(index, 1);
+			$scope.filterValues[index] = "";
+			queryService.queryObject.ByVariableFilter.splice(index, 1);
+		} else {
+			$scope.byVariableSelection[index] = "";
+			$scope.filterValues[index] = "";
+			$scope.filterType[index] = "";
+			queryService.queryObject.ByVariableFilter = [];
+		}
 	};
 	
 });
