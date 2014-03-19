017f59a99ad162ef15974c811ee5f4ed5aeb0b4b
Shweta <Shweta@129.63.17.163>
2014-03-19 19:10:11 -0400

    added support for adding new project(aside from right)and downloading json from queryObjectEditor

--- AWS/app/aws/project/projectAdditionController.js
+++ AWS/app/aws/project/projectAdditionController.js
@@ -1,8 +1,22 @@
-/**
- * This controller controls the queryObject (json ) Editor
- */
-angular.module('aws.project', [])
+angular.module('aws.project')
 .controller("projectAdditionController", function($scope, queryService){
 	
-			
+	
+	$scope.$watch('projectName', function(){
+		var project = $scope.projectName;
+	});
+	
+	$scope.$watch('userName', function(){
+		var user = $scope.userName;
+	});
+	
+	
+	 $scope.saveNewProjectToDatabase = function(){
+		
+		 console.log("saving to database");
+		 
+		// queryService.
+		 
+	 };
+	
 });
