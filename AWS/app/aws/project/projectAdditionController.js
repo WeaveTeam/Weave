3c1c7f304718674931b54e041155387f477088a9
Shweta <Shweta@129.63.17.163>
2014-03-20 15:58:55 -0400

    removed conflict from ant build.launch

--- AWS/app/aws/project/projectAdditionController.js
+++ AWS/app/aws/project/projectAdditionController.js
@@ -22,6 +22,7 @@ angular.module('aws.project')
         	var countString = fileCount.toString();
         	console.log("fileUploaded", e.targetScope.file);
         	$scope.uploadStatus = countString + " files uploaded";
+        	queryObjectJsons.push(e.targetScope.file);
 //        	var jsonObject = JSON.parse(e.targetScope.file);
 //        	var qOTitle= jsonObject.title;
 //        	$scope.uploadStatus = $scope.uploadStatus.concat(qOTitle + " uploaded") + "\n";
@@ -31,19 +32,19 @@ angular.module('aws.project')
 	
 	 $scope.saveNewProjectToDatabase = function(){
 		
-		 console.log("saving to database");
+		 console.log("jsons", queryObjectJsons);
 		 var queryObjectTitle = []; //array of titles extracted from the queryObjectJsons
 		 var queryObjectContent = [];//array of stringifed json objects
 		 
 		 
-		 for(var i in queryObjectJsons){
-			 
-			var currentTitle = queryObjectJsons[i].title;//get title
-			queryObjectTitle.push(currentTitle);
-			var singleQueryObject = JSON.stringify(quesryObjectJsons[i]); //stringify object
-			queryObjectContent.push(singleQueryObject);
-			 
-			}
+//		 for(var i in queryObjectJsons){
+//			 
+//			var currentTitle = queryObjectJsons[i].title;//get title
+//			queryObjectTitle.push(currentTitle);
+//			var singleQueryObject = JSON.stringify(quesryObjectJsons[i]); //stringify object
+//			queryObjectContent.push(singleQueryObject);
+//			 
+//			}
 		 
 		 
 		// queryService.addQueryObjectToProject(user, project, queryObjectTitle, queryObjectContent);
