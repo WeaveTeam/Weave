8b2f6f6dd175191d0c3d6aef5005625831926187
shwetapurushe <shwetapurushe@gmail.com>
2014-08-06 10:53:50 -0400

    renamed a service and added a new runScriptService

--- AWS/app/aws/bioWeave/BioWeaveController.js
+++ AWS/app/aws/bioWeave/BioWeaveController.js
@@ -3,28 +3,29 @@
  */
 var bioWeave_mod = angular.module('aws.bioWeave', []);
 
-bioWeave_mod.controller("BioWeaveController", function($scope,runScriptService){
+bioWeave_mod.controller("BioWeaveController", function($scope,algorithmObjectService){
 
 });
 
-bioWeave_mod.controller('AlgoObjectListController', function($scope,runScriptService ){
-	$scope.runScriptService = runScriptService;
+bioWeave_mod.controller('AlgoObjectListController', function($scope,algorithmObjectService ){
+	$scope.algorithmObjectService = algorithmObjectService;
 	
-	//retrive list of algorithm Objects
-	runScriptService.getListOfAlgoObjects();
+	//retrieve list of algorithm Objects
+	algorithmObjectService.getListOfAlgoObjects();
 	
 });
 
-bioWeave_mod.controller('InputParamsController', function($scope, runScriptService){
+bioWeave_mod.controller('InputParamsController', function($scope, algorithmObjectService){
 	//pulls in the external template for use in ng-include in the main BioWeaveManager.html
 	$scope.inputParamsHTMLTpl = {url: 'aws/bioWeave/parameterInputPanel.html'};
 	
-	$scope.runScriptService = runScriptService;
+	$scope.algorithmObjectService = algorithmObjectService;
 	
 });
 
-bioWeave_mod.controller('ResultsViewController', function($scope, runScriptService){
+bioWeave_mod.controller('ResultsViewController', function($scope, algorithmObjectService){
+	//pulls in the external template for use in ng-include in the main BioWeaveManager.html
 	$scope.resultViewHTMLTpl = {url: 'aws/bioWeave/resultViewPanel.html'};
 	
-	$scope.runScriptService = runScriptService;
+	$scope.algorithmObjectService = algorithmObjectService;
 });
