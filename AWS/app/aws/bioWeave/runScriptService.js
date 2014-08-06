19546ea3c5309ecd16d7a08f761256e689d3cb38
shwetapurushe <shwetapurushe@gmail.com>
2014-08-06 11:18:21 -0400

    injected the new service in index.html and added a run button

--- AWS/app/aws/bioWeave/runScriptService.js
+++ AWS/app/aws/bioWeave/runScriptService.js
@@ -1,7 +1,19 @@
 /**
  * This service handles the running of scripts and handling their results
+ * acts as a wrapper on the client side that handles requests and responses of scripts
  */
+var computationServiceURL = '/WeaveAnalystServices/ComputationalServlet';
+
 angular.module('aws.bioWeave')
 .service('runScriptService', ['$q', '$rootScope', function($q, scope){
+	var that = this;
+	this.data= {};
+	
+	/**
+	 * this function takes a list of algorithm Objects and their respective scripts and runs them in their respective engines (R, Python, STATA etc)
+	 */
+	this.runScript = function(algorithmObjects, scriptNames){
+		
+	};
 	
 }]);
