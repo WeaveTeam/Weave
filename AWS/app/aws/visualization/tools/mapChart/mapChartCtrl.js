a27552663fe4343e80e3a842c614b53b694ee59c
shwetapurushe <shwetapurushe@gmail.com>
2014-08-18 15:04:44 -0400

    added maptool widget

--- AWS/app/aws/visualization/tools/mapChart/mapChartCtrl.js
+++ AWS/app/aws/visualization/tools/mapChart/mapChartCtrl.js
@@ -1,6 +1,14 @@
-AnalysisModule.controller("MapCtrl", function($scope, queryService){
+AnalysisModule.controller("MapCtrl", function($scope, queryService, WeaveService){
 	
 	$scope.service = queryService;
+	$scope.WeaveService = WeaveService;
 	
 	queryService.getGeometryDataColumnsEntities(true);
+	
+	$scope.$watch(function(){
+		return queryService.queryObject.MapTool;
+	}, function(){
+		WeaveService.MapTool(queryService.queryObject.MapTool);
+	}, true);
+	
 });
