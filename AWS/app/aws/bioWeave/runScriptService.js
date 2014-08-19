8f9cbd5a9336eb88aface7e105ddcdb4ad0c7b51
shwetapurushe <shwetapurushe@gmail.com>
2014-08-19 18:20:44 -0400

    wrapper for sending algo objects and scripts to server

---
+++ AWS/app/aws/bioWeave/runScriptService.js
@@ -0,0 +1,67 @@
+/**
+ * This service handles the running of scripts and handling their results
+ * acts as a wrapper on the client side that handles requests and responses of scripts
+ */
+var computationURL = '/WeaveAnalystServices/ComputationalServlet';
+
+angular.module('aws.bioWeave')
+.service('runScriptService', ['$q', '$rootScope', function($q, scope){
+	var that = this;
+	this.data= {};
+	
+	/**
+	 * this function takes a list of algorithm Objects and their respective scripts and runs them in their respective engines (R, Python, STATA etc)
+	 */
+	this.runScript = function(algorithmObjects, scriptNames){
+		console.log("reached the runService", algorithmObjects, scriptNames);
+		
+		//processing the objects to send only what is required
+		for(var r in algorithmObjects)
+		{
+			var scriptName = scriptNames[r];
+			var inputParams = algorithmObjects[r].inputParams;
+			var ids = [];
+			var params = [];
+				
+			for(var t in inputParams)
+				{
+					if(inputParams[t].param_name == 'input_data' ){//picking out the data
+						ids = inputParams[t].param_user_value;
+						for(var o in ids){
+							var id  = angular.fromJson(ids[o]).id;
+							ids[o] = id;
+						}
+					}
+					
+					else{
+						//params.push(inputParams[t].param_user_value);//rest of the input Params
+						var param_name = inputParams[t].param_name;
+						var param_value = inputParams[t].param_user_value;
+						params.push({
+							name: param_name,
+							value: param_value 
+						});
+					}
+						
+					
+				}
+			
+			
+			var deferred = $q.defer();
+			aws.queryService(computationURL, 'runScript', [scriptName, ids, params], function(result){
+				
+				console.log("script result returned", result);
+				
+				
+				scope.$safeApply(function() {
+					deferred.resolve(result);
+				});
+			});
+		}
+		
+	};
+	
+	
+	
+	
+}]);
