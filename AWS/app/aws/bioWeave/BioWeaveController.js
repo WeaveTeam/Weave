b726ac79370f919a79a9d201a0509b15e04f62d7
shwetapurushe <shwetapurushe@gmail.com>
2014-08-19 18:32:30 -0400

    contains all the controllers for the main bioWeave template

---
+++ AWS/app/aws/bioWeave/BioWeaveController.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+var bioWeave_mod = angular.module('aws.bioWeave', []);
+
+bioWeave_mod.controller("BioWeaveController", function($scope,algorithmObjectService){
+
+	$scope.algorithmObjectService = algorithmObjectService;
+	algorithmObjectService.getDataTableList(true);//we do this again, because we dont want to depend on the Analysis tab for loading the datatables
+	
+});
+
+bioWeave_mod.controller('AlgoObjectListController', function($scope,algorithmObjectService ){
+	$scope.algorithmObjectService = algorithmObjectService;
+	
+	//retrieve list of algorithm Objects
+	algorithmObjectService.getListOfAlgoObjects();
+	
+});
+
+bioWeave_mod.controller('InputParamsController', function($scope, algorithmObjectService, runScriptService){
+	//pulls in the external template for use in ng-include in the main BioWeaveManager.html
+	$scope.inputParamsHTMLTpl = {url: 'aws/bioWeave/parameterInputPanel.html'};
+	
+	$scope.algorithmObjectService = algorithmObjectService;
+	
+	$scope.collectAlgoObjects = function(){
+		var titles = [];//using titles to retrive corresponding script names
+		
+		for(var f in algorithmObjectService.data.algorithmMetadataObjects){
+			titles[f] = algorithmObjectService.data.algorithmMetadataObjects[f].title;
+		}
+		
+		algorithmObjectService.getScripts(titles);
+			
+//		$scope.$watch(function(){
+//			return algorithmObjectService.data.chosenScripts;
+//		}, function(){
+//			console.log("got the scripts", algorithmObjectService.data.chosenScripts);
+//		});
+		
+		//runScriptService.runScript(algorithmObjectService.data.algorithmMetadataObjects, algorithmObjectService.data.chosenScripts);
+	};
+	
+});
+
+bioWeave_mod.controller('ResultsViewController', function($scope, algorithmObjectService){
+	//pulls in the external template for use in ng-include in the main BioWeaveManager.html
+	$scope.resultViewHTMLTpl = {url: 'aws/bioWeave/resultViewPanel.html'};
+	
+	$scope.algorithmObjectService = algorithmObjectService;
+});
