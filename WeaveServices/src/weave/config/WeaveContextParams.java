6d28fc92e5a131751bbbebcd8ba16f5ad6b50ac3
adufilie <andy.dufilie@gmail.com>
2014-08-29 23:23:05 -0400

    Revert "Added support for relative paths to Jetty"
    This reverts commit 36088f629d07f07f194b673604d5003ac4bca66b.

--- WeaveServices/src/weave/config/WeaveContextParams.java
+++ WeaveServices/src/weave/config/WeaveContextParams.java
@@ -59,41 +59,25 @@ public class WeaveContextParams
 	 */
 	private WeaveContextParams(ServletContext context) throws ServletException
 	{
-		String contextPath = context.getRealPath(".");
 		String[] paths = context.getInitParameter("docrootPath").split("\\|");
 		for (int i = 0; i < paths.length; i++)
 		{
-			File docrootFile = new File(contextPath, paths[i]);
-			docrootPath = docrootFile.getAbsolutePath().replace('\\', '/') + "/";
-			if (docrootFile.isDirectory())
+			docrootPath = context.getRealPath(paths[i]).replace('\\', '/') + "/";
+			if (new File(docrootPath).isDirectory())
 				break;
 			docrootPath = null;
 		}
 		if (docrootPath == null)
 			throw new ServletException("ERROR: Docroot unable to be determined from servlet context path: " + context.getRealPath(""));
 		System.out.println("Docroot set to "+docrootPath);
-
-		paths = context.getInitParameter("configPath").split("\\|");
-		for(int i = 0; i < paths.length; i++ )
-		{
-			File weaveConfigFile = new File(contextPath, paths[i]);
-			configPath = weaveConfigFile.getAbsolutePath().replace('\\', '/') + "/";
-			if (weaveConfigFile.isDirectory())
-				break;
-			configPath = null;
-		}
-		if( configPath == null )
-			throw new ServletException("ERROR: configPath unable to be determined from servlet context path: " + context.getRealPath(""));
-		System.out.println("configPath set to " + configPath);
-
+		
+		configPath = context.getRealPath(context.getInitParameter("configPath")).replace('\\', '/');
 		uploadPath = configPath + "/upload/";
 		rServePath = context.getInitParameter("RServePath");
 		
 		// if RServePath is not specified, keep it empty
 		if (!Strings.isEmpty(rServePath))
 			rServePath = context.getRealPath(rServePath).replace('\\', '/');
-		
-		System.out.println("RServePath: " + rServePath);
 		
 		// make sure folders exist
 		new File(configPath).mkdirs();
