7ca57bbb98dda79b7820a33787062f74868eb753
shwetapurushe <shwetapurushe@gmail.com>
2014-09-04 18:40:40 -0400

    added function for getting base64session state of weave

--- AWS/app/aws/visualization/weave/WeaveModule.js
+++ AWS/app/aws/visualization/weave/WeaveModule.js
@@ -1,5 +1,5 @@
 var weave_mod = angular.module('aws.WeaveModule', []);
-AnalysisModule.service("WeaveService", function(queryService) {
+AnalysisModule.service("WeaveService", function() {
 	
 	this.weave;
 	var ws = this;
@@ -158,6 +158,15 @@ AnalysisModule.service("WeaveService", function(queryService) {
 			if(! angular.isUndefined(ws.weave))
 			ws.weave.setSessionState(['CSVDataSource'], {keyColName : "fips"});
 		}
+	};
+	
+	this.getSessionState = function()
+	{
+		return ws.weave.path().getValue("\
+		        var e = new 'mx.utils.Base64Encoder'();\
+		        e.encodeBytes( Class('weave.Weave').createWeaveFileContent(true) );\
+		        return e.drain();\
+		    ");
 	};
 });
 
