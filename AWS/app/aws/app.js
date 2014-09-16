ec9287b133304292b860aa67caed09dc7d7cea17
shwetapurushe <shwetapurushe@gmail.com>
2014-09-16 13:59:20 -0400

    error log (aside to modal) changed appearance using css

--- AWS/app/aws/app.js
+++ AWS/app/aws/app.js
@@ -82,11 +82,24 @@ app.controller('AWSController', function($scope, $route, $location) {
 	
 });
 //controller for the error log that is universal to all tabs
-app.controller('analystErrorLogCtrl', function($scope){
-	$scope.errorAside = {
-		title : "Error Log"	
+app.controller('analystErrorLogCtrl', function($scope, $modal){
+	
+	$scope.openErrorLog = function(){
+		$modal.open({
+			 backdrop: false,
+	         backdropClick: true,
+	         dialogFade: true,
+	         keyboard: true,
+	         templateUrl: 'analystErrorLog.html',
+	         controller: 'errorLogInstanceCtrl',
+	         windowClass : 'erroLog-modal'
+		});
 	};
 	
+}).controller('errorLogInstanceCtrl', function($rootScope, $scope, $modalInstance){
+	 $scope.close = function () {
+		 $modalInstance.close();
+	 };
 });
 //var navbar_ctrl = function($scope, $route, $location) {
 //	$scope.$route = $route;
