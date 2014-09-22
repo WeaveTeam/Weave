8a1dfb67f96ee94f9be73f4e4d2712d6b9009fea
shwetapurushe <shwetapurushe@gmail.com>
2014-09-22 10:54:25 -0400

    converted dialog to modal(save output button) (in progress)

--- AWS/app/aws/analysis/dialogCtrl.js
+++ AWS/app/aws/analysis/dialogCtrl.js
@@ -1,35 +1,36 @@
 AnalysisModule.controller('DialogController', function ($scope, $modal, queryService) {
-//	$scope.opts = {
-//		 backdrop: false,
-//          backdropClick: true,
-//          dialogFade: true,
-//          keyboard: true,
-//          templateUrl: 'aws/analysis/myModalContent.html',
-//          controller: 'DialogInstanceCtrl',
-//          resolve:
-//          {
-//                     // projectEntered: function() {return angular.copy(projectEntered);},
-//                     // queryTitleEntered : function(){return angular.copy(queryTitleEntered);}
-//          }
-//	};
-//
-//    $scope.saveVisualizations = function (projectEntered, queryTitleEntered) {
-//    	
-//    	$modal.open($scope.opts).then(function(params) {
-//    	  if(params){
-//    		  console.log("finally got project as ", params.projectEntered);
-//    		  console.log("qo", params.queryTitleEntered);
-//    		  queryService.getSessionState(params);
-//    	  }
-//      });
-//    };
-//  })
-//  .controller('DialogInstanceCtrl', function ($scope, $modalInstance, projectEntered, queryTitleEntered) {
-//	  $scope.close = function (projectEntered, queryTitleEntered) {
-//		  var params = {
-//				  projectEntered : projectEntered,
-//				  queryTitleEntered : queryTitleEntered
-//		  };
-//		  $modalInstance.close(params);
-//    };
-  });
+	$scope.projectEntered2 = "Hello";
+	$scope.opts = {
+		 backdrop: false,
+          backdropClick: true,
+          dialogFade: true,
+          keyboard: true,
+          templateUrl: 'aws/analysis/myModalContent.html',
+          controller: 'DialogInstanceCtrl',
+          resolve:
+          {
+                      projectEntered: function() {return $scope.projectEntered;},
+                      queryTitleEntered : function(){return $scope.queryTitleEntered;}
+          }
+	};
+
+    $scope.saveVisualizations = function (projectEntered, queryTitleEntered) {
+    	
+    	var saveQueryObjectInstance = $modal.open($scope.opts);
+    	saveQueryObjectInstance.result.then(function(params){//this takes only a single object
+    	console.log("checking", params);
+    		
+    	});
+    };
+    
+  })
+ .controller('DialogInstanceCtrl', function ($scope, $modalInstance, projectEntered, queryTitleEntered) {
+	  $scope.close = function (projectEntered, queryTitleEntered) {
+		  console.log("checking2", projectEntered, queryTitleEntered);
+		  var params = {
+				  projectEntered : projectEntered,
+				  queryTitleEntered : queryTitleEntered
+		  };
+		  $modalInstance.close(params);
+    };
+});
