f1c09aabb8f997815ccb5b64c15cd026e6c33f01
shwetapurushe <shwetapurushe@gmail.com>
2014-09-22 16:59:21 -0400

    project addition modal (in progress)

--- AWS/app/aws/project/projectAdditionController.js
+++ AWS/app/aws/project/projectAdditionController.js
@@ -1,100 +1,109 @@
+var hoo;
 var projectModule = angular.module('aws.project');
-projectModule.controller("projectAdditionController", function($scope, projectService){
+projectModule.controller("projectAdditionController", function($scope, $modal, projectService){
+	$scope.projectService = projectService;
 	
-	//var project = "";
-	//var projectDescription = "";
-	//var user = "";
-	projectService.data.uploadStatus = "No file uploaded";
+	//projectService.data.uploadStatus = "No file uploaded";
 	//projectService.data.queryObjectJsons = []; //array of uploaded queryObject jsons
 	//projectService.data.queryObjectTitles = [];
-	var fileCount = 0;
-    $scope.fileUpload;
+	var queryObjectJsons = [];
+	var queryObjectTitles = [];
 	
-    //$scope.projtDescription = ""; 
-//	$scope.$watch('projectName', function(){
-//		 project = $scope.projectName;
-//	});
+//
+//	$scope.$watch('fileUpload', function(n, o) {
+//              $scope.fileUpload.then(function(result) {
+//            	$scope.uploadStatus = "";
+//            	
+//            	//TODO find way to retain file uploaded for state retention
+//            	queryObjectJsons = [];//reset
+//            	queryObjectTitles = [];//reset before every upload
+//            	
+//            	$scope.uploadStatus = result.filename+ " uploaded";  
+//            	queryObjectJsons.push(result.contents);//filling up the json array
+//                var jsonObject = JSON.parse(result.contents);
+//                console.log("json",jsonObject);
+//                queryObjectTitles.push(jsonObject.title);
+//              });
+//            
+//          }, true);
 //	
-//	$scope.$watch('userName', function(){
-//		 user = $scope.userName;
-//	});
-//	
-	$scope.$watch('fileUpload', function(n, o) {
-            if ($scope.fileUpload && $scope.fileUpload.then) {
-              $scope.fileUpload.then(function(result) {
-               // $scope.uploadStatus = "";
-            	projectService.projectBundle.uploadStatus = "";
-            	
-            	//TODO find way to retain file uploaded for state retention
-            	projectService.projectBundle.queryObjectJsons = [];//reset
-            	projectService.projectBundle.queryObjectTitles = [];//reset before every upload
-            	
-            	projectService.projectBundle.uploadStatus = result.filename+ " uploaded";  
-                projectService.projectBundle.queryObjectJsons.push(result.contents);//filling up the json array
-                var jsonObject = JSON.parse(result.contents);
-                console.log("json",jsonObject);
-                projectService.projectBundle.queryObjectTitles.push(jsonObject.title);
-              });
-            }
-          }, true);
-	
-	
-	
-	 $scope.saveNewProjectToDatabase = function(){
-		
-		 console.log("jsons", projectService.projectBundle.queryObjectJsons);
-		 console.log("titles", projectService.projectBundle.queryObjectTitles);
-		 console.log("userName", projectService.projectBundle.userName);
-		 
-		 if(angular.isUndefined(projectService.projectBundle.userName))
-			 projectService.projectBundle.userName = "Awesome User";
-		 projectService.projectBundle.resultVisualizations = null;//when queryObjects will be added with a project, generated visualizations will be null
-				 
-		 projectService.createNewProject(projectService.projectBundle);
-		
-		 
-	 };
-	
-});
-
-
-projectModule.controller('pjtAdditonModal', function($scope, $modal){
-	$scope.pjtModalOptions = {//TODO find out how to push error log to bottom of page
+//	 $scope.saveNewProjectToDatabase = function(){
+//		
+//		 console.log("jsons", queryObjectJsons);
+//		 console.log("titles", queryObjectTitles);
+//		 console.log("userName",$scope.userNameEntered);
+//		 
+//		 if(angular.isUndefined($scope.userNameEntered))
+//			 $scope.userNameEntered = "Awesome User";
+//		 $scope.resultVisualizations = null;//when queryObjects will be added with a project, generated visualizations will be null
+//				 
+//		 projectService.createNewProject(userNameEntered,
+//				 						 projectNameEntered,
+//				 						 projectDescriptionEntered,
+//				 						 queryObjectTitles,
+//				 						 queryObjectJsons);
+//		
+//		 
+//	 };
+	 
+	 //communicating with the modal
+	 $scope.pjtModalOptions = {//TODO find out how to push error log to bottom of page
 			 backdrop: true,
 	         backdropClick: true,
 	         dialogFade: true,
 	         keyboard: true,
 	         templateUrl: 'aws/project/projectAdditionPanel.html',
-	         controller: 'pjtAddtionInstanceCtrl'
+	         controller: 'pjtAddtionInstanceCtrl',
+	         resolve :{
+	        	 projectNameEntered : function(){return $scope.projectNameEntered;},
+	        	 userNameEntered : function(){return $scope.userNameEntered;},
+	        	 projectDescriptionEntered : function(){return $scope.projectDescriptionEntered;}
+	        	 
+	         }
 		};
 	
 	$scope.openAdditionPanel = function(){
-		$modal.open($scope.pjtModalOptions);
+		var saveNewProject = $modal.open($scope.pjtModalOptions);
+		
+		saveNewProject.result.then(function(additionParams){
+			 console.log("jsons", queryObjectJsons);
+			 console.log("titles", queryObjectTitles);
+			 console.log("userName", additionParams.userNameEntered);
+			 
+//			 projectService.createNewProject(additionParams.userNameEntered,
+//					 						 additionParams.projectNameEntered,
+//					 						 additionParams.projectDescriptionEntered,
+//					 						 queryObjectTitles,
+//					 						 queryObjectJsons);
+			 
+		});
 	};
-	
 });
 
-projectModule.controller('pjtAddtionInstanceCtrl', function($rootScope,$scope, $modalInstance, projectService) {
-	//this is the scope of the modal window that actually opens up
+//Modal instance controller
+projectModule.controller('pjtAddtionInstanceCtrl', function($rootScope,$scope, $modalInstance, projectNameEntered,projectDescriptionEntered, userNameEntered ) {
+	$scope.uploadStatus = "No file uploaded";
 	
-	$scope.projectService = projectService;
-	//saves a new project (collection of query objects to the server)
-	$scope.saveNewProjectToDatabase = function(){
+	$scope.uploadedQueryObject = {
+			filename : "",
+			content : ""
+	};
+	$scope.$watch('uploadedQueryObject.filename', function(){
 		
-//		 console.log("jsons", projectService.projectBundle.queryObjectJsons);
-//		 console.log("titles", projectService.projectBundle.queryObjectTitles);
-//		 console.log("userName", projectService.projectBundle.userName);
-//		 
-//		 if(angular.isUndefined(projectService.projectBundle.userName))
-//			 projectService.projectBundle.userName = "Awesome User";
-//		 projectService.projectBundle.resultVisualizations = null;//when queryObjects will be added with a project, generated visualizations will be null
-//				 
-//		 projectService.createNewProject(projectService.projectBundle);
+		if($scope.uploadedQueryObject)
+			{
+				console.log("got file name",$scope.uploadedQueryObject.filename );
+			}
 		
-		 
-	 };
+	},true);
 	
-	$scope.close = function () {
-		 $modalInstance.close();
+	$scope.close = function (projectNameEntered,projectDescriptionEntered) {
+		var additionParams = {
+				projectNameEntered : projectNameEntered,
+				userNameEntered :userNameEntered,
+				projectDescriptionEntered : projectDescriptionEntered
+		};
+		
+		 $modalInstance.close(additionParams);
 	 };
 });
