874da9f91b62cddde7cebfa80e4f340c1b8dca6f
shwetapurushe <shwetapurushe@gmail.com>
2014-09-25 16:25:44 -0400

    uploading Qos to prjt addition modal(+ing/-ing)

--- AWS/app/aws/project/projectAdditionController.js
+++ AWS/app/aws/project/projectAdditionController.js
@@ -1,4 +1,3 @@
-var hoo;
 var projectModule = angular.module('aws.project');
 projectModule.controller("projectAdditionController", function($scope, $modal, projectService){
 	$scope.projectService = projectService;
@@ -6,8 +5,8 @@ projectModule.controller("projectAdditionController", function($scope, $modal, p
 	//projectService.data.uploadStatus = "No file uploaded";
 	//projectService.data.queryObjectJsons = []; //array of uploaded queryObject jsons
 	//projectService.data.queryObjectTitles = [];
-	var queryObjectJsons = [];
-	var queryObjectTitles = [];
+	//var queryObjectJsons = [];
+	//var queryObjectTitles = [];
 	
 //
 //	$scope.$watch('fileUpload', function(n, o) {
@@ -46,10 +45,11 @@ projectModule.controller("projectAdditionController", function($scope, $modal, p
 //		 
 //	 };
 	 
+	//options needed for creating the modal instance window
 	 //communicating with the modal
 	 $scope.pjtModalOptions = {//TODO find out how to push error log to bottom of page
 			 backdrop: true,
-	         backdropClick: true,
+	         backdropClick: false,
 	         dialogFade: true,
 	         keyboard: true,
 	         templateUrl: 'aws/project/projectAdditionPanel.html',
@@ -62,19 +62,22 @@ projectModule.controller("projectAdditionController", function($scope, $modal, p
 	         }
 		};
 	
+	//button click event that creates the modal
 	$scope.openAdditionPanel = function(){
 		var saveNewProject = $modal.open($scope.pjtModalOptions);
 		
-		saveNewProject.result.then(function(additionParams){
-			 console.log("jsons", queryObjectJsons);
-			 console.log("titles", queryObjectTitles);
+		//called when modal is being closed
+		saveNewProject.result.then(function(additionParams){//then function takes a single object
+			 console.log("jsons", additionParams.uploadedObjects.queryObjectJsons);
+			 console.log("titles", additionParams.uploadedObjects.queryObjectTitles);
 			 console.log("userName", additionParams.userNameEntered);
+			 
 			 
 //			 projectService.createNewProject(additionParams.userNameEntered,
 //					 						 additionParams.projectNameEntered,
 //					 						 additionParams.projectDescriptionEntered,
-//					 						 queryObjectTitles,
-//					 						 queryObjectJsons);
+//					 						 additionParams.queryObjectTitles,
+//					 						 additionParams.queryObjectJsons);
 			 
 		});
 	};
@@ -82,28 +85,67 @@ projectModule.controller("projectAdditionController", function($scope, $modal, p
 
 //Modal instance controller
 projectModule.controller('pjtAddtionInstanceCtrl', function($rootScope,$scope, $modalInstance, projectNameEntered,projectDescriptionEntered, userNameEntered ) {
-	$scope.uploadStatus = "No file uploaded";
+	$scope.uploadStatus = "";
 	
-	$scope.uploadedQueryObject = {
-			filename : "",
-			content : ""
+	//object representation of a SINGLE file uploaded, changed everytime a file is uploaded
+	$scope.uploaded = {
+			QueryObject : {
+				filename : "",
+				content : ""			
+			}
 	};
-	$scope.$watch('uploadedQueryObject.filename', function(){
+	
+	$scope.uploadedObjects = {
+			
+		queryObjectJsons : [],//contains the content of all query objects uploaded (json strings)
+		queryObjectTitles : []//contains the titles of all query Objects uploaded
+	};
+	
+	
+	var queryObjectJsons = [];
+	$scope.queryObjectTitles = [];
+	//whenever a file is uploaded
+	$scope.$watch('uploaded.QueryObject.filename', function(){
 		
-		if($scope.uploadedQueryObject)
+		if($scope.uploaded.QueryObject.filename)
 			{
-				console.log("got file name",$scope.uploadedQueryObject.filename );
+				//check if the file had been uploaded before
+				if($.inArray($scope.uploaded.QueryObject.filename, $scope.uploadedObjects.queryObjectTitles) == -1)
+					{
+						//managing the title of queryObject (json )uploaded
+						var title = $scope.uploaded.QueryObject.filename;
+						$scope.uploadedObjects.queryObjectTitles.push(title);
+						
+						//managing the content of queryObject (json )uploaded
+						var content = $scope.uploaded.QueryObject.content;
+						$scope.uploadedObjects.queryObjectJsons.push(content);
+						
+						
+						var countUploaded = $scope.uploadedObjects.queryObjectTitles.length;
+						$scope.uploadStatus = countUploaded + " file(s) uploaded";
+					}
 			}
 		
-	},true);
+	});
 	
-	$scope.close = function (projectNameEntered,projectDescriptionEntered) {
+	//called when save button is hit.;
+	$scope.saveQueryObjects = function (projectNameEntered,projectDescriptionEntered, userNameEntered) {
 		var additionParams = {
 				projectNameEntered : projectNameEntered,
 				userNameEntered :userNameEntered,
-				projectDescriptionEntered : projectDescriptionEntered
+				projectDescriptionEntered : projectDescriptionEntered,
+				uploadedObjects : $scope.uploadedObjects
 		};
 		
 		 $modalInstance.close(additionParams);
+	 };
+	 
+	 $scope.remove = function(file){
+		 //removes the file from the uploaded collection
+		 $scope.uploadedObjects.queryObjectTitles.splice($.inArray(file, $scope.uploadedObjects.queryObjectTitles), 1);
+		 var countUploaded = $scope.uploadedObjects.queryObjectTitles.length;
+		 $scope.uploadStatus = countUploaded + " file(s) uploaded";
+		 if(countUploaded == 0)
+			 $scope.uploadStatus = "";
 	 };
 });
