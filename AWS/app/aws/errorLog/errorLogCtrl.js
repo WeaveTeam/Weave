6a3aa3bcd2753c8b7774588e61fd54cd6a74a68b
franckamayou <franckamayou@gmail.com>
2014-10-01 15:11:38 -0400

    changed error log from dialog to panel

--- AWS/app/aws/errorLog/errorLogCtrl.js
+++ AWS/app/aws/errorLog/errorLogCtrl.js
@@ -5,41 +5,21 @@
 
 var errorLogModule = angular.module('aws.errorLog', []);
 
-errorLogModule.controller('analystErrorLogCtrl', function($scope, $modal, errorLogService){
+errorLogModule.controller('analystErrorLogCtrl', function($scope, errorLogService){
 	$scope.errorLogService = errorLogService;
-	$scope.openErrorLog = function(){
-		$modal.open($scope.errorLogService.errorLogModalOptions);
-	};
 });
-
-errorLogModule.controller('errorLogInstanceCtrl', function($rootScope, $scope, $modalInstance, errorLogService){
-	$scope.logs = errorLogService.logs;
-	
-	$scope.close = function () {
-		 $modalInstance.close();
-	 };
-});
-
 
 errorLogModule.service('errorLogService',[function(){
 	
-	this.errorLogModalOptions = {//TODO find out how to push error log to bottom of page
-			 backdrop: true,
-	         backdropClick: true,
-	         dialogFade: true,
-	         keyboard: true,
-	         templateUrl: 'aws/errorLog/analystErrorLog.html',
-	         controller: 'errorLogInstanceCtrl',
-	         windowClass : 'erroLog-modal'
-		};
-	
 	this.logs = "";
+	this.showErrorLog = false;
+
 	/**
 	 *this is the function that will be used over all tabs to log errors to the error log
 	 *@param the string you want to log to the error log
 	 */
 	this.logInErrorLog = function(error){
-		this.logs= this.logs.concat("\n" + error + new Date().toLocaleTimeString());
+		this.logs += error + "\n" + this.logs;
 	};
 	
 }]);
