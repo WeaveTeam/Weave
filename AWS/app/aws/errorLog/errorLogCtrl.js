123d3ede43a380e8129ee1e4443ebdee70ed52dd
shwetapurushe <shwetapurushe@gmail.com>
2014-10-06 15:43:26 -0400

    error log modal used (disabled panel TODO: decide)

--- AWS/app/aws/errorLog/errorLogCtrl.js
+++ AWS/app/aws/errorLog/errorLogCtrl.js
@@ -5,11 +5,32 @@
 
 var errorLogModule = angular.module('aws.errorLog', []);
 
-errorLogModule.controller('analystErrorLogCtrl', function($scope, errorLogService){
+errorLogModule.controller('analystErrorLogCtrl', function($scope,$modal, errorLogService){
 	$scope.errorLogService = errorLogService;
+	$scope.openErrorLog = function(){
+		$modal.open($scope.errorLogService.errorLogModalOptions);
+	};
+});
+
+errorLogModule.controller('errorLogInstanceCtrl', function($rootScope, $scope, $modalInstance, errorLogService){
+	$scope.logs = errorLogService.logs;
+	
+	$scope.close = function () {
+		 $modalInstance.close();
+	 };
 });
 
 errorLogModule.service('errorLogService',[function(){
+	
+	this.errorLogModalOptions = {//TODO find out how to push error log to bottom of page
+			 backdrop: true,
+	         backdropClick: true,
+	         dialogFade: true,
+	         keyboard: true,
+	         templateUrl: 'aws/errorLog/analystErrorLog.html',
+	         controller: 'errorLogInstanceCtrl',
+	         windowClass : 'erroLog-modal'
+		};
 	
 	this.logs = "";
 	this.showErrorLog = false;
@@ -19,7 +40,7 @@ errorLogModule.service('errorLogService',[function(){
 	 *@param the string you want to log to the error log
 	 */
 	this.logInErrorLog = function(error){
-		this.logs += error + "\n" + this.logs;
+		this.logs += error + "\n" + this.logs +  new Date().toLocaleTimeString();
 	};
 	
 }]);
