7f31a18460848967eecefa2957e24300559d874a
shwetapurushe <shwetapurushe@gmail.com>
2014-10-09 17:58:14 -0400

    special select directive completed (TODO work on css)

--- AWS/app/aws/utils/aws-select-directive.js
+++ AWS/app/aws/utils/aws-select-directive.js
@@ -7,15 +7,13 @@ var utils_Module = angular.module('aws.utils', []);
 utils_Module.directive('awsSelectDirective', function factory(){
 	
 	var directiveDefnObj= {
-			restrict : 'E', //restricts the directive to a specific directive declaration style.in this case as element
-			templateUrl : 'aws/utils/special_select.tpl.html',
+			restrict : 'A', //restricts the directive to a specific directive declaration style.in this case as element
 			scope : {
-				arrayOfObjects : '='
+				collection : '='
 			},
+			templateUrl : 'aws/utils/special_select.tpl.html',
 			controller : function($scope, $compile){
 				$scope.searchOption;
-				console.log("columns",$scope.arrayOfObjects);
-				//$scope.arrayOfObjects = [1,2,3];
 				$scope.check = function(obj){
 					console.log("selected item", obj);
 					$scope.selectedItem = angular.copy(obj);
@@ -25,24 +23,15 @@ utils_Module.directive('awsSelectDirective', function factory(){
 					if(angular.isDefined($scope.selectedItem))
  					  $scope.selectedItem.title = "";
 				};
+				
 			},
-			//the scope is that of the parent controller
-			//elem :The jQLite wrapped element on which the directive is applied.  
-			//attrs : any attributes that may have been applied on the directive element for e.g.<aws-select-directive style = "padding-top: 5px"></aws-select-directive>
 			link: function(scope, elem, attrs){
 				console.log("scope in directive", scope);
-//				console.log("elem in directive", elem);
-//				console.log("attrs in directive", attrs);
 				
 				scope.columnfilter = attrs.columnfilter;
-//				console.log("columnFilter", scope.columnfilter);
 			}
 	};
 	
 	return directiveDefnObj;
 });
 
-utils_Module.controller('specialCtrl', function($scope){
-	$scope.adds = [1,2,4];
-	console.log("scope in main ctrl", $scope);
-});
