44f8ec048f61da937217c5ef7150f6f10f76b429
shwetapurushe <shwetapurushe@gmail.com>
2014-10-28 18:07:05 -0400

    code doc and removing redundant code

--- AWS/app/src/analysis/analysis.js
+++ AWS/app/src/analysis/analysis.js
@@ -52,38 +52,8 @@ AnalysisModule.service('AnalysisService', function() {
 											    						description : 'Filter data by Variables',
 											    						category : 'datafilter'
 						    										}
-										   },
-										   tool_list : [
-											         	{
-											        		id : 'BarChartTool',
-											        		title : 'Bar Chart Tool',
-											        		template_url : 'src/visualization/tools/barChart/bar_chart.tpl.html',
-											        		description : 'Display Bar Chart in Weave',
-											        		category : 'visualization',
-											        		enabled : false
-
-											        	}, {
-											        		id : 'MapTool',
-											        		title : 'Map Tool',
-											        		template_url : 'src/visualization/tools/mapChart/map_chart.tpl.html',
-											        		description : 'Display Map in Weave',
-											        		category : 'visualization',
-											        		enabled : false
-											        	}, {
-											        		id : 'DataTableTool',
-											        		title : 'Data Table Tool',
-											        		template_url : 'src/visualization/tools/dataTable/data_table.tpl.html',
-											        		description : 'Display a Data Table in Weave',
-											        		category : 'visualization',
-											        		enabled : false
-											        	}, {
-											        		id : 'ScatterPlotTool',
-											        		title : 'Scatter Plot Tool',
-											        		template_url : 'src/visualization/tools/scatterPlot/scatter_plot.tpl.html',
-											        		description : 'Display a Scatter Plot in Weave',
-											        		category : 'visualization',
-											        		enabled : false
-											        	}]
+										   }
+										  
 	};
 	
 
@@ -119,7 +89,7 @@ AnalysisModule.controller('AnalysisCtrl', function($scope, $filter, queryService
 	
 	var queryObjectTreeData = buildTree2(queryService.queryObject);
 	queryObjectTreeData.title = "Query Object";
-	console.log(queryObjectTreeData);
+	//console.log(queryObjectTreeData);
 	
 	$('#queryObjTree').dynatree({
 		minExpandLevel: 1,
