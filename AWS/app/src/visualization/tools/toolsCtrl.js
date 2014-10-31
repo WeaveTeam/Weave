5fa07e4365f9f2ebc73f02edc367a98ff0bb23b4
shwetapurushe <shwetapurushe@gmail.com>
2014-10-31 19:07:55 -0400

    new list for viz widget tools in Analysis module

--- AWS/app/src/visualization/tools/toolsCtrl.js
+++ AWS/app/src/visualization/tools/toolsCtrl.js
@@ -2,27 +2,28 @@ AnalysisModule.controller("toolsCtrl", function($scope, queryService, WeaveServi
 	
 	$scope.queryService = queryService;
 	$scope.WeaveService = WeaveService;
+	$scope.AnalysisService = AnalysisService;
 	
-	$scope.tool_list = [
-		         	{
-		        		id : 'BarChartTool',
-		        		title : 'Bar Chart Tool',
-		        		template_url : 'src/visualization/tools/barChart/bar_chart.tpl.html'
-
-		        	}, {
-		        		id : 'MapTool',
-		        		title : 'Map Tool',
-		        		template_url : 'src/visualization/tools/mapChart/map_chart.tpl.html'
-		        	}, {
-		        		id : 'DataTableTool',
-		        		title : 'Data Table Tool',
-		        		template_url : 'src/visualization/tools/dataTable/data_table.tpl.html',
-		        		description : 'Display a Data Table in Weave'
-		        	}, {
-		        		id : 'ScatterPlotTool',
-		        		title : 'Scatter Plot Tool',
-		        		template_url : 'src/visualization/tools/scatterPlot/scatter_plot.tpl.html',
-		        		description : 'Display a Scatter Plot in Weave'
-		        	},
-		        	];
+//	$scope.tool_list = [
+//		         	{
+//		        		id : 'BarChartTool',
+//		        		title : 'Bar Chart Tool',
+//		        		template_url : 'src/visualization/tools/barChart/bar_chart.tpl.html'
+//
+//		        	}, {
+//		        		id : 'MapTool',
+//		        		title : 'Map Tool',
+//		        		template_url : 'src/visualization/tools/mapChart/map_chart.tpl.html'
+//		        	}, {
+//		        		id : 'DataTableTool',
+//		        		title : 'Data Table Tool',
+//		        		template_url : 'src/visualization/tools/dataTable/data_table.tpl.html',
+//		        		description : 'Display a Data Table in Weave'
+//		        	}, {
+//		        		id : 'ScatterPlotTool',
+//		        		title : 'Scatter Plot Tool',
+//		        		template_url : 'src/visualization/tools/scatterPlot/scatter_plot.tpl.html',
+//		        		description : 'Display a Scatter Plot in Weave'
+//		        	},
+//		        	];
 });
