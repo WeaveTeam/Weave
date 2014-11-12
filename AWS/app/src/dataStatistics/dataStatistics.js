2064a7567a4b003bf3696ba793be325a869f4705
shwetapurushe <shwetapurushe@gmail.com>
2014-11-12 11:43:48 -0500

    added the data statistics module

---
+++ AWS/app/src/dataStatistics/dataStatistics.js
@@ -0,0 +1,9 @@
+/**
+ * controllers for the 'Data' tab
+ */
+
+var dataStatsModule = angular.module('aws.dataStatistics', []);
+
+dataStatsModule.controller('dataStatsCtrl', function(){
+	console.log("reached the stats controller");
+});
