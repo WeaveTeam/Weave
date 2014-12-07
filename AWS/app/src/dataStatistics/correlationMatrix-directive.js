17c50da8e70ff0f5d5d2d821dee6df76317f9587
shwetapurushe <shwetapurushe@gmail.com>
2014-12-07 15:41:51 -0500

    correlation matrix calculated and drawn in d3

--- AWS/app/src/dataStatistics/correlationMatrix-directive.js
+++ AWS/app/src/dataStatistics/correlationMatrix-directive.js
@@ -2,7 +2,8 @@
  * this directive contains the UI and logic for the correlation Matrix
  */
 
-dataStatsModule.directive('correlationMatrix', function factory(){
+dataStatsModule.directive('correlationMatrix',['pearsonCoeff','spearmanCoeff','queryService', 'statisticsService',
+                                               function factory(pearsonCoeff, spearmanCoeff, queryService, statisticsService){
 	var directiveDefnObj= {
 			restrict : 'E', //restricts the directive to a specific directive declaration style.in this case as element
 			scope : {//isolated scope, its parent is the scope of the correlation matrix tab in dataStatsMain.tpl.html
@@ -10,27 +11,18 @@ dataStatsModule.directive('correlationMatrix', function factory(){
 			},
 			templateUrl: 'src/dataStatistics/correlationMatrices.tpl.html',
 			//this is the scope against the templateUrl i.e.src/dataStatistics/correlationMatrices.tpl.htm
-			controller : function($scope, $compile, pearsonCoeff,spearmanCoeff, queryService){
-
+			controller : function($scope, $compile, pearsonCoeff,spearmanCoeff, queryService, statisticsService){
+				$scope.correlationMatrix= null;
 				$scope.selectedCoeff = {label : "", scriptName : ""};
 				
 				$scope.availableCoeffList = [pearsonCoeff, spearmanCoeff];
-				$scope.correlationMatrix = [];
-				
+				$scope.statisticsService = statisticsService;
 				
 				//calculates the correlation coefficient in R
 				$scope.calculateCoefficient = function(scriptName){
 					
-					queryService.getDataFromServer($scope.data).then(function(sucess){
-						
-						if(success){
-							queryService.runScript(scriptName).then(function(resultData){
-								if(resultData){
-									that.cache.calculatedStats = resultData;
-								}
-							});
-						}
-					});
+					statisticsService.calculateStats(scriptName, queryService.cache.numericalColumns, true);
+
 				};
 				
 			},
@@ -38,49 +30,74 @@ dataStatsModule.directive('correlationMatrix', function factory(){
 			//elem :The jQLite wrapped element on which the directive is applied.  
 			//attrs : any attributes that may have been applied on the directive element for e.g.<aws-select-directive style = "padding-top: 5px"></aws-select-directive>
 			link: function(scope, elem, attrs){
+				
+				//**************************************CORRELATON MATRIX VIZ ***********************************
+				
 				var array1 = [1,6,9,4,5];
 				var array2= [2,7,8,3,10];
-				var data = [array1, array2];
+				//var data = [array1, array2];
+				var data = [];
 				
-				var margin = {top: 20, right: 20, bottom: 20, left: 20},
-			    width = 250 - margin.left - margin.right,//260
-			    height = 250 - margin.top - margin.bottom;
+				var drawCorrelationHeatMap = function(data){
+					//TODO remove previous svg
+					var margin = {top: 20, right: 20, bottom: 20, left: 20},
+				    width = 250 - margin.left - margin.right,
+				    height = 250 - margin.top - margin.bottom;
 
-		
-				var rowScale = d3.scale.linear()
-				    .range([0, width])
-				    .domain([0,data[0].length]);
-		
-				var colScale = d3.scale.linear()
-				    .range([0, height])
-				    .domain([0,data.length]);
-		
-				var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
-		
-				var colorScale = d3.scale.linear()
-				     .domain([0, 5, 10])
-				     .range([colorLow, colorMed, colorHigh]);
-		
-		
-				var mysvg = d3.select("#corM").append("svg")
-				    .attr("width", width + margin.left + margin.right)
-				    .attr("height", height + margin.top + margin.bottom)
-				    .append("g")
-				    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		
-				var rows = mysvg.selectAll(".row")
-				             .data(data)
-				           .enter().append("svg:g")
-				             .attr("class", "row"); 
-		
-				var cols = rows.selectAll(".cell")
-				    .data(function (d,i) { return d.map(function(a) { return {value: a, rows: i}; } ) })
-				           .enter().append("svg:rect")
-				             .attr("x", function(d, i) { return rowScale(i); })
-				             .attr("y", function(d, i) { return colScale(d.rows); })
-				             .attr("width", rowScale(1))
-				             .attr("height", colScale(1))
-				             .style("fill", function(d) { return colorScale(d.value); });
+			
+					var rowScale = d3.scale.linear()
+					    .range([0, width])
+					    .domain([0,data[0].length]);
+			
+					var colScale = d3.scale.linear()
+					    .range([0, height])
+					    .domain([0,data.length]);
+			
+					var colorLow = 'green', colorMed = 'yellow', colorHigh = 'red';
+			
+					var colorScale = d3.scale.linear()
+					     .domain([0, 0.5, 1])//TODO parameterize this according to the matrix  
+					     .range([colorLow, colorMed, colorHigh]);
+			
+			
+					var mysvg = d3.select("#corM").append("svg")
+					    .attr("width", width + margin.left + margin.right)
+					    .attr("height", height + margin.top + margin.bottom)
+					    .append("g")
+					    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			
+					var rows = mysvg.selectAll(".row")
+					             .data(data)
+					           .enter().append("svg:g")
+					             .attr("class", "row"); 
+			
+					var cols = rows.selectAll(".cell")
+					    .data(function (d,i) { return d.map(function(a) { return {value: a, rows: i}; } ) })
+					           .enter().append("svg:rect")
+					             .attr("x", function(d, i) { return rowScale(i); })
+					             .attr("y", function(d, i) { return colScale(d.rows); })
+					             .attr("width", rowScale(1))
+					             .attr("height", colScale(1))
+					             .style("fill", function(d) { return colorScale(d.value); });
+					
+					
+				};
+				
+				//**************************************SCOPE WATCHES***********************************
+				scope.$watch(function(){
+					return scope.statisticsService.cache.correlationMatrix;
+				}, function(newVal, oldVal){
+					if(scope.correlationMatrix){
+						if(!newVal)
+							return;
+						else{
+							data = statisticsService.cache.correlationMatrix;
+							console.log("data", data);
+							if(data.length > 0)
+							drawCorrelationHeatMap(data[0]);//TODO fix this
+						}
+					}
+				});
 
 //				var sampleSVG = d3.select("#corM")
 //			    .append("svg")
@@ -107,5 +124,5 @@ dataStatsModule.directive('correlationMatrix', function factory(){
 	};
 	
 	return directiveDefnObj;
-});
+}]);
 
