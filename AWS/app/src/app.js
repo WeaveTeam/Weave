19e43fbd438ce310a3b9c5680526e6d3b44b98a5
shwetapurushe <shwetapurushe@gmail.com>
2014-12-09 15:39:44 -0500

    replaced ng-route with ui-router library

--- AWS/app/src/app.js
+++ AWS/app/src/app.js
@@ -11,8 +11,9 @@ var app = angular.module('aws', [//'aws.router', // for app structure (can be cl
                                  //'ui.slider',
                                  'ui.bootstrap',
                                  'ui.sortable', // Shweta Needs, comes from angular-strap???
-                                 'ngRoute',
+                                 //'ngRoute',
                                  'ngGrid', // Angular UI library
+                                 'ui.router',
                                  'mk.editablespan', // Directive for editing values.
                                  'aws.configure', //Both script and metadata managers
                                  'aws.dataStatistics',
@@ -40,36 +41,83 @@ app.run(['$rootScope', function($rootScope){
     	}
 	};
 }])
-.config(function($parseProvider, $routeProvider){
+.config(function($stateProvider, $urlRouterProvider, $parseProvider) {
+	
 	$parseProvider.unwrapPromises(true);
 	
-	// Also from Amith's UI
-	$routeProvider.when('/analysis', {
-		templateUrl : 'src/analysis/analysis.tpl.html',
-		controller : 'AnalysisCtrl',
-		activetab : 'analysis'
-	}).when('/metadata', {
-		templateUrl : 'src/configure/metadata/metadataManager.html',
-		controller : 'MetadataManagerCtrl',
-		activetab : 'metadata'
-	}).when('/script_management', {
-		templateUrl : 'src/configure/script/scriptManager.html',
-		controller : 'ScriptManagerCtrl',
-		activetab : 'script_management'
-	}).when('/project_management', {
-		templateUrl : 'src/project/projectManagementPanel.html',
-		controller : 'ProjectManagementCtrl',
-		activetab : 'project_management'
-	}).when('/data_stats', {
-		templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
-		controller : 'dataStatsCtrl',
-		activetab : 'data_stats'
-	}).otherwise({
-        redirectTo: '/analysis'
-    });
-
+	$urlRouterProvider.otherwise("/index.html");
+	
+	$stateProvider
+		.state('metadata', {
+			url:'/metadata',
+			templateUrl : 'src/configure/metadata/metadataManager.html',
+			controller: 'MetadataManagerCtrl',
+			data: {}
+		})
+	    .state('script_management', {
+	    	url:'/scripts',
+	    	templateUrl : 'src/configure/script/scriptManager.html',
+	    	controller : 'ScriptManagerCtrl'
+	    })
+	    .state('analysis', {
+	    	url:'/analysis',
+	    	templateUrl : 'src/analysis/analysis.tpl.html',
+	    	controller: 'AnalysisCtrl',
+	    	data:{}
+	    })
+	    .state('project', {
+	    	url:'/projects',
+	    	templateUrl : 'src/project/projectManagementPanel.html',
+	    	controller : 'ProjectManagementCtrl'
+	    })
+	    .state('data_stats',{
+	    	url:'/dataStatistics',
+	    	templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
+    		controller : 'dataStatsCtrl'
+	    })
+	    .state('data_stats.summary_stats',{
+	    	url:'/summary_stats',
+	    	templateUrl: 'src/dataStatistics/summary_stats.tpl.html'
+	    })
+	    .state('data_stats.correlations', {
+	    	url:'/correlations',
+	    	templateUrl : 'src/dataStatistics/correlation_matrices.tpl.html'
+	    });
+		
+	    
 });
 
+/**********************Using ng-route***************************************/
+//.config(function($parseProvider, $routeProvider){
+//	$parseProvider.unwrapPromises(true);
+//	
+//	// Also from Amith's UI
+//	$routeProvider.when('/analysis', {
+//		templateUrl : 'src/analysis/analysis.tpl.html',
+//		controller : 'AnalysisCtrl',
+//		activetab : 'analysis'
+//	}).when('/metadata', {
+//		templateUrl : 'src/configure/metadata/metadataManager.html',
+//		controller : 'MetadataManagerCtrl',
+//		activetab : 'metadata'
+//	}).when('/script_management', {
+//		templateUrl : 'src/configure/script/scriptManager.html',
+//		controller : 'ScriptManagerCtrl',
+//		activetab : 'script_management'
+//	}).when('/project_management', {
+//		templateUrl : 'src/project/projectManagementPanel.html',
+//		controller : 'ProjectManagementCtrl',
+//		activetab : 'project_management'
+//	}).when('/data_stats', {
+//		templateUrl : 'src/dataStatistics/dataStatisticsMain.tpl.html',
+//		controller : 'dataStatsCtrl',
+//		activetab : 'data_stats'
+//	}).otherwise({
+//        redirectTo: '/analysis'
+//    });
+//
+//});
+/**********************Using ng-route***************************************/
 
 angular.module('aws.directives', ['aws.directives.dualListBox',
                                   'aws.directives.fileUpload',
@@ -86,14 +134,8 @@ app.value('scriptManagementURL', '/WeaveAnalystServices/ScriptManagementServlet'
 app.value('computationServiceURL', '/WeaveAnalystServices/ComputationalServlet');
 
 // From Amith's UI
-app.controller('AWSController', function($scope, $route, $location, authenticationService) {
-	$scope.$route = $route;
+app.controller('AWSController', function($scope, authenticationService) {
+	//$scope.$route = $route;
 	$scope.authenticationService = authenticationService;
 
 });
-
-
-
-//var navbar_ctrl = function($scope, $route, $location) {
-//	$scope.$route = $route;
-//};
