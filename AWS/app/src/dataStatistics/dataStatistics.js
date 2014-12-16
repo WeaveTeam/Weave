2d1aa44ee389ef0576634543ee71325f0c16cdd6
shwetapurushe <shwetapurushe@gmail.com>
2014-12-16 12:30:36 -0500

    separation of ctrls and re-factored for new result format

--- AWS/app/src/dataStatistics/dataStatistics.js
+++ AWS/app/src/dataStatistics/dataStatistics.js
@@ -61,16 +61,23 @@ dataStatsModule.service('statisticsService', ['queryService', 'QueryHandlerServi
 }]);
 
 
-//********************CONTROLLERS
+//********************CONTROLLERS***************************************************************
 dataStatsModule.controller('dataStatsCtrl', function($q, $scope, 
 													 queryService, statisticsService, runQueryService, 
 													 scriptManagementURL){
-	$scope.queryService = queryService;
-	$scope.statisticsService = statisticsService;
-	$scope.columnDefinitions = [];
-
 	//this is the table selected for which its columns and their respective data statistics will be shown
 	$scope.datatableSelected = queryService.queryObject.dataTable.title;
+});
+
+dataStatsModule.controller('summaryStatsController', function($q, $scope, 
+		 													  queryService, statisticsService, runQueryService, 
+		 													  scriptManagementURL){
+	
+	$scope.queryService = queryService;
+	$scope.statisticsService = statisticsService;
+	$scope.columnDefinitions = [];//populates the stats grid
+
+	
 	
 	//wrote this in the controller so that even if user edits stats metadata on the fly, no need to refresh application
 	var getStatsMetadata = function(statsScript){
@@ -86,34 +93,50 @@ dataStatsModule.controller('dataStatsCtrl', function($q, $scope,
 	};
 	//getting the metadata which specifies the stats to be displayed
 	var statsInputMetadata;
-	//getStatsMetadata("getStatistics.R");
+	getStatsMetadata("getStatistics.R");
 	
 	//calculating stats
-	//statisticsService.calculateStats("getStatistics.R", queryService.cache.numericalColumns,"SummaryStatistics", true);
+	statisticsService.calculateStats("getStatistics.R", queryService.cache.numericalColumns,"SummaryStatistics", true);
 	
-
-	//as soon as results are returned construction of the STATS DATAGRID
-	$scope.$watch(function(){
-		return $scope.statisticsService.cache.summaryStats;
-	}, function(){
-		if($scope.statisticsService.cache.summaryStats.length > 0){
+	var setData = function(data){
+		$scope.statsData = data;
+	};
+	
+	$scope.statsData = [];//the array that gets populated by the Column statistics
+	//defines the main grid that displays descriptive column statistics
+	$scope.statsGrid = { 
+	        data: 'statsData',
+	        enableRowSelection: true,
+	        enableCellEdit: true,
+	        columnDefs: 'columnDefinitions',
+	        multiSelect : false
+	 };
+	
+	//this function populates the Summary statistics grid
+	var constructSummaryStatsGrid = function(){
+		if($scope.statisticsService.cache.summaryStats.resultData){
 			var data = [];
-			var oneStat = $scope.statisticsService.cache.summaryStats[0];//hack
+			var oneStat = $scope.statisticsService.cache.summaryStats.resultData;//hack
+			//getting column titles
+			var columnTitles = getColumnTitles($scope.queryService.cache.numericalColumns);
 			
-			for(var x = 0; x < oneStat.length; x++){
+			for(var x = 0; x < oneStat.length; x++){// x number of numerical columns
 				
 				var oneStatsGridObject = {};
-				for(var y = 0; y < statsInputMetadata.inputs.length; y++){
-					statsInputMetadata.inputs[y].value = $scope.statisticsService.cache.summaryStats[y][x];
+				for(var y = 0; y < statsInputMetadata.inputs.length; y++){//x number of metadata objects
 					
-					oneStatsGridObject[statsInputMetadata.inputs[y].param] = statsInputMetadata.inputs[y].value;
+					if(statsInputMetadata.inputs[y].param == 'ColumnName'){//since the dataprovider for this entry is different i.e. columnTitles
+						oneStatsGridObject[statsInputMetadata.inputs[y].param] = columnTitles[x];
+						continue;
+					}
 					
+					oneStatsGridObject[statsInputMetadata.inputs[y].param] = $scope.statisticsService.cache.summaryStats.resultData[x][y-1];
 				}
 				
 				data.push(oneStatsGridObject);
 				
 				//during the last iteration TODO confirm if this is the right place for the loop
-				console.log(x);
+				//console.log(x);
 				if(x == (oneStat.length - 1)){
 					$scope.columnDefitions = [];
 					for(var z = 0; z < statsInputMetadata.inputs.length; z++){
@@ -130,19 +153,25 @@ dataStatsModule.controller('dataStatsCtrl', function($q, $scope,
 			
 			setData(data);
 		}
-	});
-	
-	var setData = function(data){
-		$scope.statsData = data;
 	};
 	
-	$scope.statsData = [];//the array that gets populated by the Column statistics
-	//defines the main grid that displays descriptive column statistics
-	$scope.statsGrid = { 
-	        data: 'statsData',
-	        enableRowSelection: true,
-	        enableCellEdit: true,
-	        columnDefs: 'columnDefinitions',
-	        multiSelect : false
-	 };
+	//convenience function to get column titles
+	var getColumnTitles = function(columns){
+		var columnTitles = [];
+		
+		for(var t=0; t < columns.length; t++){
+			columnTitles[t] = columns[t].title;
+		}
+		
+		return columnTitles;
+	};
+	
+	/*******************************************************SCOPE watches***********************************************/
+	//as soon as results are returned construction of the STATS DATAGRID
+	$scope.$watch(function(){
+		return $scope.statisticsService.cache.summaryStats;
+	}, function(){
+		constructSummaryStatsGrid();
+	});
+	
 });
