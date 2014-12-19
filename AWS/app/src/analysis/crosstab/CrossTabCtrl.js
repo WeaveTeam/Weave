fa0f461cdf01123438fc9cae6e11bd8b308a36c9
franckamayou <franckamayou@gmail.com>
2014-12-19 16:03:44 -0500

    adding ui in crossTab tab

--- AWS/app/src/analysis/crosstab/CrossTabCtrl.js
+++ AWS/app/src/analysis/crosstab/CrossTabCtrl.js
@@ -1,6 +1,37 @@
 //main analysis controller
 AnalysisModule.controller('CrossTabCtrl', function($scope, $filter, queryService, AnalysisService, WeaveService, QueryHandlerService, $window,statisticsService ) {
 	
-	$scope.queryService = queryService;
+	queryService.getDataTableList(true);
 	
+	queryService.crossTabQuery = {};
+	
+	$scope.queryService = queryService;
+
+	
+	$scope.getItemId = function(item) {
+		return item.id;
+	};
+	
+	$scope.getItemText = function(item) {
+		if(queryService.queryObject.properties.displayAsQuestions)
+			return item.description || item.title;
+		return item.title;
+	};
+	
+	//datatable
+	$scope.getDataTable = function(term, done) {
+		var values = queryService.cache.dataTableList;
+		done($filter('filter')(values, {title:term}, 'title'));
+	};
+	
+	
+	$scope.$watch("queryService.queryObject.dataTable.id", function() {
+		queryService.getDataColumnsEntitiesFromId(queryService.queryObject.dataTable.id, true);
+	});
+	
+	//Indicator
+	 $scope.getIndicators = function(term, done) {
+			var columns = queryService.cache.columns;
+			done($filter('filter')(columns,{columnType : 'indicator',title:term},'title'));
+	};
 });
