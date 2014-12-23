e954def7310cf50ac9256ec466baadd07742e639
shwetapurushe <shwetapurushe@gmail.com>
2014-12-22 20:13:11 -0500

    added support for sparklines using d3 service

--- AWS/app/src/dataStatistics/sparklines-directive.js
+++ AWS/app/src/dataStatistics/sparklines-directive.js
@@ -2,8 +2,8 @@
  * this directive contains the UI and logic for the sparklines drawn for each numerical column
  */
 
-dataStatsModule.directive('sparkLine',['queryService', 'statisticsService',
-                                               function factory(queryService, statisticsService){
+dataStatsModule.directive('sparkLine',['queryService', 'statisticsService','d3Service',
+                                               function factory(queryService, statisticsService, d3Service){
 	var directiveDefnObj= {
 			restrict: 'E',
 			scope : {
@@ -12,10 +12,18 @@ dataStatsModule.directive('sparkLine',['queryService', 'statisticsService',
 			},
 			templateUrl: 'src/dataStatistics/sparklines_directive_content.tpl.html',
 			controller : function($scope, queryService, statisticsService){
-				
+				$scope.statisticsService = statisticsService;
 			},
 			link: function(scope, elem, attrs){
+				//TODO confirm if this is the rightway of doing it. 
 			
+				scope.data = {breaks:[0,0.25,0.5,0.75,1.0], counts:{"sampleColumn" :[6,2,2,8,2]}};//breaks and counts for a single column
+				
+				var dom_element_to_append_to = document.getElementById('singleContainer');
+				
+				//service call for drawing sparklines
+				d3Service.drawSparklines(dom_element_to_append_to, scope.data);
+				
 			}//end of link function
 	};
 	
