3e2b60c8c0e2ec2daee68ca064287dc245cd942e
shwetapurushe <shwetapurushe@gmail.com>
2015-01-23 14:25:14 -0500

    moved dialog controller into analysis.js (TODO delete dialogCtrl.js)

--- AWS/app/src/analysis/analysis.js
+++ AWS/app/src/analysis/analysis.js
@@ -679,3 +679,47 @@ AnalysisModule.controller("ScriptsSettingsCtrl", function($scope, queryService, 
 	};
 	
 });
+
+AnalysisModule.controller('DialogController', function ($scope, $modal, queryService, WeaveService) {
+	$scope.projectEntered2 = "Hello";
+	$scope.opts = {
+		 backdrop: false,
+          backdropClick: true,
+          dialogFade: true,
+          keyboard: true,
+          templateUrl: 'src/analysis/savingOutputsModal.html',
+          controller: 'DialogInstanceCtrl',
+          resolve:
+          {
+                      projectEntered: function() {return $scope.projectEntered;},
+                      queryTitleEntered : function(){return $scope.queryTitleEntered;}
+          }
+	};
+
+    $scope.saveVisualizations = function (projectEntered, queryTitleEntered) {
+    	
+    	var saveQueryObjectInstance = $modal.open($scope.opts);
+    	saveQueryObjectInstance.result.then(function(params){//this takes only a single object
+    	//console.log("params", params);
+    		queryService.getSessionState(params);
+    		
+    	});
+    };
+    
+  //clears the session state
+	$scope.clearSessionState = function(){
+		WeaveService.clearSessionState();
+	};
+	
+    
+  });
+
+AnalysisModule.controller('DialogInstanceCtrl', function ($scope, $modalInstance, projectEntered, queryTitleEntered) {
+	  $scope.close = function (projectEntered, queryTitleEntered) {
+		  var params = {
+				  projectEntered : projectEntered,
+				  queryTitleEntered : queryTitleEntered
+		  };
+		  $modalInstance.close(params);
+  };
+});
