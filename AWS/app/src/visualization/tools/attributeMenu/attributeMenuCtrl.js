d1a3c13100b16cc8816c91b1778e4d7fb12d9ef3
shwetapurushe <shwetapurushe@gmail.com>
2015-03-31 14:38:00 -0400

    attribute column control next pass (TODO refresh)

--- AWS/app/src/visualization/tools/attributeMenu/attributeMenuCtrl.js
+++ AWS/app/src/visualization/tools/attributeMenu/attributeMenuCtrl.js
@@ -1,11 +1,10 @@
 /**
  * controls the attribute menu visualization tool  widget
  */
-var x ;
 AnalysisModule.controller("AttributeMenuCtrl", function($scope, WeaveService, $timeout){
 
 	$scope.WeaveService = WeaveService;
-	
+
 	$scope.$watch('tool.enabled', function(){
 		if($scope.tool.enabled)
 			$scope.openTools = WeaveService.listOfTools();
@@ -14,7 +13,7 @@ AnalysisModule.controller("AttributeMenuCtrl", function($scope, WeaveService, $t
 	$scope.$watch('tool.selectedVizTool', function(){
 		//console.log("tools selected", $scope.tool.selectedVizTool);
 		if($scope.tool.selectedVizTool)
-			$scope.vizAttributeColl = WeaveService.getSelectableAttributes($scope.tool.selectedVizTool);
+			$scope.vizAttributeColl = WeaveService.getSelectableAttributes($scope.tool.title, $scope.tool.selectedVizTool);
 	});
 	
 	$scope.$watch('tool', function() {
@@ -28,6 +27,9 @@ AnalysisModule.controller("AttributeMenuCtrl", function($scope, WeaveService, $t
 		//check for tha attrbite selected
 		if($scope.tool.vizAttribute && $scope.tool.selectedVizTool && $scope.tool.chosenAttribute)
 			//set the attribute in weave
-			WeaveService.setVizAttribute($scope.tool.selectedVizTool,$scope.tool.vizAttribute,$scope.tool.chosenAttribute);
+			WeaveService.setVizAttribute($scope.tool.title,
+										  $scope.tool.selectedVizTool,
+										  $scope.tool.vizAttribute,
+										  $scope.tool.chosenAttribute);
 	};
 });
