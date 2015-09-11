fff0915723037ed5ef6481d0853a06b3db7931c6
Philip "digitalfox" Kovac <pkovac@cs.uml.edu>
2015-09-11 17:19:35 -0400

    Reset weave_version.txt to contain 'Custom'

--- set_version.sh
+++ set_version.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 VERSION_FILE_PATH="./WeaveUISpark/src/weave/weave_version.txt"
 if [ -z `git status --porcelain -uno` ]; then
     echo Automated Build `git rev-parse --short HEAD`, `date` > $VERSION_FILE_PATH;
+else
+    echo Custom > $VERSION_FILE_PATH
 fi
