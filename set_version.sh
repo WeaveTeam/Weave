8c64fd99a2d0e621d14cdbd14a6795307c9fcb13
Philip "digitalfox" Kovac <pkovac@cs.uml.edu>
2015-09-11 17:08:25 -0400

    Make version script overwrite, not append.

--- set_version.sh
+++ set_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION_FILE_PATH="./WeaveUISpark/src/weave/weave_version.txt"
 if [ -z `git status --porcelain -uno` ]; then
-    echo Automated Build `git rev-parse --short HEAD`, `date` >> $VERSION_FILE_PATH;
+    echo Automated Build `git rev-parse --short HEAD`, `date` > $VERSION_FILE_PATH;
 fi
